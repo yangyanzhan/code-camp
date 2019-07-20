dd7900a6ee1d21c66a69e1e6b3989cbf6bb09c69
yangyanzhan <yangyanzhan@baidu.com>
2019-07-21 00:37:26 +0800

    add leetcode solution for problem 1

---
+++ leetcode/1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+__author__ = "yangyanzhan"
+__email__ = "yangyanzhan@gmail.com"
+__url__ = "https://github.com/yangyanzhan/code-camp"
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums = [(nums[i], i) for i in range(len(nums))]
+        nums.sort()
+        start = 0
+        end = len(nums) - 1
+        v1 = nums[start][0]
+        v2 = nums[end][0]
+        while start < end:
+            sum = v1 + v2
+            if sum == target:
+                return [nums[start][1], nums[end][1]]
+            elif sum < target:
+                start += 1
+                v1 = nums[start][0]
+            else:
+                end -= 1
+                v2 = nums[end][0]
+        return None
