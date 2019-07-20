def9ece95f8a17e371c38d49847bad575cacc607
yangyanzhan <yangyanzhan@baidu.com>
2019-07-21 00:35:13 +0800

    add solution template

---
+++ tools/gen-template.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp tools/template.py run.py
---
+++ tools/template.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+__author__ = "yangyanzhan"
+__email__ = "yangyanzhan@gmail.com"
+__url__ = "https://github.com/yangyanzhan/code-camp"
+
+
