e66ecd690f85f0af8fada95edaa6d999264a61ef
yangyanzhan <yangyanzhan@baidu.com>
2019-07-28 12:06:17 +0800

    add solution for leetcode problem 5

---
+++ leetcode/c++/5.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if (n == 0) {
+            return "";
+        }
+        int max_len = 1;
+        int res_i = 0;
+        int res_j = 0;
+        bool *palin_cache = new bool[n * n];
+        for (int j = 0; j < n; j++) {
+            palin_cache[j * n + j] = true;
+            for (int i = 0; i < j; i++) {
+                bool is_palin = false;
+                if (i == j - 1) {
+                    is_palin = s[i] == s[j];
+                } else {
+                    is_palin = s[i] == s[j] && palin_cache[(i + 1) * n + j - 1];
+                }
+                palin_cache[i * n + j] = is_palin;
+                if (is_palin) {
+                    int curr_len = j - i + 1;
+                    if (curr_len > max_len) {
+                        max_len = curr_len;
+                        res_i = i;
+                        res_j = j;
+                    }
+                }
+            }
+        }
+        delete[] palin_cache;
+        return s.substr(res_i, max_len);
+    }
+};
---
+++ leetcode/python/5.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+__author__ = "yangyanzhan"
+__email__ = "yangyanzhan@gmail.com"
+__url__ = "https://github.com/yangyanzhan/code-camp"
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n == 0:
+            return ""
+        max_len = 1
+        res_i = 0
+        res_j = 0
+        is_palin = {}
+        for j in range(n):
+            is_palin[(j, j)] = True
+            for i in range(j):
+                if i == j - 1:
+                    palin = s[i] == s[j]
+                else:
+                    palin = s[i] == s[j] and is_palin[(i + 1, j - 1)]
+                is_palin[(i, j)] = palin
+                if palin:
+                    curr_len = j - i + 1
+                    if curr_len > max_len:
+                        max_len = curr_len
+                        res_i = i
+                        res_j = j
+        return s[res_i: res_j + 1]
