8ee4ab3391ad0e33f670a2dceb4239f75c01ff08
yangyanzhan <yangyanzhan@baidu.com>
2019-08-25 14:35:25 +0800

    add solution for leetcode problem 8

---
+++ leetcode/c++/8.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int myAtoi(string str) {
+        int n = str.size();
+        // check for sign
+        int64_t sign = 1;
+        int digit_idx = -1;
+        for (int i = 0; i < n; i++) {
+            char ch = str[i];
+            if (ch == ' ') {
+                continue;
+            }
+            if (ch == '+') {
+                sign = 1;
+                digit_idx = i + 1;
+                break;
+            }
+            if (ch == '-') {
+                sign = -1;
+                digit_idx = i + 1;
+                break;
+            }
+            int digit = ch - '0';
+            if (0 <= digit && digit <= 9) {
+                sign = 1;
+                digit_idx = i;
+                break;
+            }
+            return 0;
+        }
+        if (digit_idx >= n || digit_idx == -1) {
+            return 0;
+        }
+        int digit = str[digit_idx] - '0';
+        if (digit < 0 || digit > 9) {
+            return 0;
+        }
+        int64_t num = 0;
+        int64_t res;
+        int64_t min = INT_MIN;
+        int64_t max = INT_MAX;
+        for (int i = digit_idx; i < n; i++) {
+            int digit = str[i] - '0';
+            if (digit < 0 || digit > 9) {
+                break;
+            }
+            num *= 10;
+            num += digit;
+            res = sign * num;
+            if (res >= max) {
+                res = max;
+                break;
+            }
+            if (res <= min) {
+                res = min;
+                break;
+            }
+        }
+        return res;
+    }
+};
---
+++ leetcode/python/8.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+__author__ = "yangyanzhan"
+__email__ = "yangyanzhan@gmail.com"
+__url__ = "https://github.com/yangyanzhan/code-camp"
+
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        intMin = int(-pow(2, 31))
+        intMax = int(pow(2, 31) - 1)
+        n = len(str)
+        sign = 1
+        digitIdx = -1
+        for i in range(n):
+            ch = str[i]
+            if ch == " ":
+                continue
+            if ch == "+":
+                sign = 1
+                digitIdx = i + 1
+                break
+            if ch == "-":
+                sign = -1
+                digitIdx = i + 1
+                break
+            digitIdx = i
+            break
+        if digitIdx < 0 or digitIdx > n:
+            return 0
+        res = 0.0
+        for i in range(digitIdx, n):
+            digit = ord(str[i]) - ord("0")
+            if digit < 0 or digit > 9:
+                break
+            res *= 10
+            res += sign * digit
+            if res >= intMax:
+                return intMax
+            if res <= intMin:
+                return intMin
+        return int(res)
