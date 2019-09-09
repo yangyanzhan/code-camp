f80575674db64c3594a3b1582ddade3edc29d082
yangyanzhan <yangyanzhan@baidu.com>
2019-09-09 20:06:34 +0800

    add solution for leetcode problem 15

---
+++ leetcode/python/15.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+__author__ = "yangyanzhan"
+__email__ = "yangyanzhan@gmail.com"
+__url__ = "https://github.com/yangyanzhan/code-camp"
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        solutions = []
+        nums.sort()
+        n = len(nums)
+        preNum1 = None
+        preNum2 = None
+        preNum3 = None
+        for i in range(n - 2):
+            num1 = nums[i]
+            if num1 == preNum1:
+                continue
+            preNum1 = num1
+            start = i + 1
+            end = n - 1
+            while start < end:
+                num2 = nums[start]
+                num3 = nums[end]
+                sum = num1 + num2 + num3
+                moveStart = False
+                moveEnd = False
+                if sum == 0:
+                    solutions.append([num1, num2, num3])
+                    moveStart = True
+                    moveEnd = True
+                elif sum < 0:
+                    moveStart = True
+                else:
+                    moveEnd = True
+                if moveStart:
+                    preNum2 = num2
+                    while start < end:
+                        start += 1
+                        num2 = nums[start]
+                        if preNum2 != num2:
+                            break
+                if moveEnd:
+                    preNum3 = num3
+                    while start < end:
+                        end -= 1
+                        num3 = nums[end]
+                        if preNum3 != num3:
+                            break
+        return solutions
