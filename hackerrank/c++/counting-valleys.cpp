cf4e34dbee63382ce8a2c02c0a1e51b101d355db
yangyanzhan <yangyanzhan@gmail.com>
2019-10-26 21:27:43 +0800

    add Yanzhan's solution to counting valleys problem on hackerrank in C++

---
+++ hackerrank/c++/counting-valleys.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int countingValleys(int n, string s) {
+    int h = 0, res = 0;
+    for (auto ch : s) {
+        if (ch == 'U') {
+            h++;
+            if (h == 0) {
+                res++;
+            }
+        } else {
+            h--;
+        }
+    }
+    return res;
+}
