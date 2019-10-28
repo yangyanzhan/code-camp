9f6f68d577d04893e6b51c921f4069b77c3593a7
yangyanzhan <yangyanzhan@gmail.com>
2019-10-28 23:48:10 +0800

    add Yanzhan's cpp solution for the greedy florist problem on hackerrank

---
+++ hackerrank/c++/greedy-florist.cpp
@@ -0,0 +1,13 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int getMinimumCost(int k, vector<int> c) {
+    sort(c.begin(), c.end());
+    reverse(c.begin(), c.end());
+    int res = 0;
+    for (int i = 0; i < c.size(); i++) {
+        res += (i / k + 1) * c[i];
+    }
+    return res;
+}
