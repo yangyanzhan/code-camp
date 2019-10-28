2cfc7842e52c497103e8f6627fd652505ccc3558
yangyanzhan <yangyanzhan@gmail.com>
2019-10-28 17:00:27 +0800

    add Yanzhan's cpp solution for the sorting bubble sort problem on hackerrank

---
+++ hackerrank/c++/sorting-bubble-sort.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+void countSwaps(vector<int> a) {
+    int count = 0;
+    for (int i = 0; i < a.size(); i++) {
+        for (int j = 0; j < a.size() - 1 - i; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+                count++;
+            }
+        }
+    }
+    cout << "Array is sorted in " << count << " swaps." << endl;
+    cout << "First Element: " << a[0] << endl;
+    cout << "Last Element: " << a[a.size() - 1] << endl;
+}
