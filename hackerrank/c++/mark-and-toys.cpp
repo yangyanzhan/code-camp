edbcdf2b1bf877131c9c563093cd8cbd6b3d806f
yangyanzhan <yangyanzhan@gmail.com>
2019-10-28 17:15:30 +0800

    add Yanzhan's cpp solution for the mark and toys problem on hackerrank

---
+++ hackerrank/c++/mark-and-toys.cpp
@@ -0,0 +1,16 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int maximumToys(vector<int> prices, int k) {
+    sort(prices.begin(), prices.end());
+    int res = 0, cost = 0;
+    for (auto price : prices) {
+        cost += price;
+        if (cost > k) {
+            break;
+        }
+        res++;
+    }
+    return res;
+}
