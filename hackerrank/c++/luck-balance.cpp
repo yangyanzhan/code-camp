7a47766718a2811941438718e6753d341b19b655
yangyanzhan <yangyanzhan@gmail.com>
2019-10-28 23:26:14 +0800

    add Yanzhan's cpp solution for the luck balance problem on hackerrank

---
+++ hackerrank/c++/luck-balance.cpp
@@ -0,0 +1,23 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int luckBalance(int k, vector<vector<int>> contests) {
+    vector<int> imps;
+    int res = 0;
+    for (auto contest : contests) {
+        bool important = contest[1] == 1;
+        int luck = contest[0];
+        if (!important) {
+            res += luck;
+        } else {
+            imps.push_back(luck);
+        }
+    }
+    sort(imps.begin(), imps.end());
+    reverse(imps.begin(), imps.end());
+    for (int i = 0; i < imps.size(); i++) {
+        res += i < k ? imps[i] : -imps[i];
+    }
+    return res;
+}
