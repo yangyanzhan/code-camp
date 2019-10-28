07d762f7765fcfff1fb5ad70d93a27812d54e1c3
yangyanzhan <yangyanzhan@gmail.com>
2019-10-28 15:30:22 +0800

    add Yanzhan's cpp solution for the new year chaos problem on hackerrank

---
+++ hackerrank/c++/new-year-chaos.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+void minimumBribes(vector<int> q) {
+    int res = 0;
+    for (int i = 0; i < q.size(); i++) {
+        int num = q[i];
+        if ((num - 1 - i) > 2) {
+            cout << "Too chaotic" << endl;
+            return;
+        }
+        for (int j = max(0, num - 1 - 2); j < i; j++) {
+            if (q[j] > num) {
+                res++;
+            }
+        }
+    }
+    cout << res << endl;
+}
