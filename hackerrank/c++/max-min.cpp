001d699795255976eba5032c4543bc09d2dfa4e5
yangyanzhan <yangyanzhan@gmail.com>
2019-10-28 23:57:30 +0800

    add Yanzhan's cpp solution for the max min problem on hackerrank

---
+++ hackerrank/c++/max-min.cpp
@@ -0,0 +1,12 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int maxMin(int k, vector<int> arr) {
+    sort(arr.begin(), arr.end());
+    int res = numeric_limits<int>::max();
+    for (int i = k - 1; i < arr.size(); i++) {
+        res = min(res, arr[i] - arr[i - (k - 1)]);
+    }
+    return res;
+}
