e1f636ad7d2c5b496967078c83b0ee1187503e67
yangyanzhan <yangyanzhan@gmail.com>
2019-10-28 14:54:09 +0800

    add Yanzhan's cpp solution for the arrays left rotation problem on hackerrank

---
+++ hackerrank/c++/arrays-left-rotation.cpp
@@ -0,0 +1,10 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+vector<int> rotLeft(vector<int> a, int d) {
+    d = d % a.size();
+    vector<int> res(a.begin() + d, a.end());
+    res.insert(res.end(), a.begin(), a.begin() + d);
+    return res;
+}
