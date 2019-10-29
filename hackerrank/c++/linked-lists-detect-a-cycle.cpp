01a1d964e1784939d3c10413e94966e9d6f262fc
yangyanzhan <yangyanzhan@gmail.com>
2019-10-29 15:20:47 +0800

    add Yanzhan's cpp solution for the linked lists detect a cycle problem on hackerrank

---
+++ hackerrank/c++/linked-lists-detect-a-cycle.cpp
@@ -0,0 +1,15 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+bool has_cycle(Node* head) {
+    auto slow = head, fast = head;
+    while (fast && fast->next) {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow) {
+            return true;
+        }
+    }
+    return false;
+}
