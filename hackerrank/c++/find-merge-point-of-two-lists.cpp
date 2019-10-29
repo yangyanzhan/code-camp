59a749b1de37de1795ad78a0978238186dc395c7
yangyanzhan <yangyanzhan@gmail.com>
2019-10-29 14:57:16 +0800

    add Yanzhan's cpp solution for the find merge point of two lists problem on hackerrank

---
+++ hackerrank/c++/find-merge-point-of-two-lists.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    auto curr1 = head1, curr2 = head2;
+    while (curr1 != curr2) {
+        if (curr1->next) {
+            curr1 = curr1->next;
+        } else {
+            curr1 = head2;
+        }
+        if (curr2->next) {
+            curr2 = curr2->next;
+        } else {
+            curr2 = head1;
+        }
+    }
+    return curr1;
+}
