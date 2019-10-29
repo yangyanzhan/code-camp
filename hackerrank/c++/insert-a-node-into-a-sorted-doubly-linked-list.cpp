9b421e20017289773225f52f963c66b504ce84d1
yangyanzhan <yangyanzhan@gmail.com>
2019-10-29 14:20:04 +0800

    add Yanzhan's cpp solution for the insert a node into a sorted doubly linked list problem on hackerrank

---
+++ hackerrank/c++/insert-a-node-into-a-sorted-doubly-linked-list.cpp
@@ -0,0 +1,35 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data) {
+    auto new_node = new DoublyLinkedListNode(data);
+    auto curr = head;
+    while (curr->next && curr->next->data < data) {
+        curr = curr->next;
+    }
+    if (!curr) {
+        return new_node;
+    }
+    if (curr->data >= new_node->data) {
+        if (curr == head) {
+            new_node->next = curr;
+            curr->prev = new_node;
+            return new_node;
+        } else {
+            curr->prev->next = new_node;
+            new_node->prev = curr->prev;
+            new_node->next = curr;
+            curr->prev = new_node;
+            return head;
+        }
+    } else {
+        if (curr->next) {
+            new_node->next = curr->next;
+            curr->next->prev = new_node;
+        }
+        new_node->prev = curr;
+        curr->next = new_node;
+        return head;
+    }
+}
