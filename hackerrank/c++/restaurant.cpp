006ac0adbff62b8272323157f9f058cf19713feb
yangyanzhan <yangyanzhan@gmail.com>
2019-10-31 15:29:29 +0800

    add Yanzhan's cpp solution for the restaurant problem on hackerrank

---
+++ hackerrank/c++/restaurant.cpp
@@ -0,0 +1,15 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int gcd(int m, int n) {
+    if (n % m == 0) {
+        return m;
+    }
+    return gcd(n % m, m);
+}
+
+int restaurant(int l, int b) {
+    int m = min(l, b), n = max(l, b), g = gcd(m, n);
+    return (m / g) * (n / g);
+}
