66627794746dcf7f4fb32cf3da27fcdcff17df31
yangyanzhan <yangyanzhan@gmail.com>
2019-11-03 15:26:14 +0800

    add Yanzhan's cpp solution for the special multiple problem on hackerrank

---
+++ hackerrank/c++/special-multiple.cpp
@@ -0,0 +1,21 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+string solve(int n) {
+    unsigned long long trial = 1;
+    while (true) {
+        unsigned long long number = 0;
+        unsigned long long tmp = trial;
+        unsigned long long base = 1;
+        while (tmp > 0) {
+            number += 9 * (tmp % 2) * base;
+            tmp /= 2;
+            base *= 10;
+        }
+        if (number % n == 0) {
+            return to_string(number);
+        }
+        trial++;
+    }
+}
