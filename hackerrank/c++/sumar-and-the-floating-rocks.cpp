82c3ba689c35e3b1772b5591734e0c33c19bdf06
yangyanzhan <yangyanzhan@gmail.com>
2019-11-04 11:47:38 +0800

    add Yanzhan's cpp solution for the sumar and the floating rocks problem on hackerrank

---
+++ hackerrank/c++/sumar-and-the-floating-rocks.cpp
@@ -0,0 +1,21 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int gcd(int a, int b) {
+    if (b % a == 0) {
+        return a;
+    }
+    return gcd(b % a, a);
+}
+int solve(int x1, int y1, int x2, int y2) {
+    if (x1 == x2) {
+        return abs(y1 - y2) - 1;
+    }
+    if (y1 == y2) {
+        return abs(x1 - x2) - 1;
+    }
+    int a = min(abs(x1 - x2), abs(y1 - y2));
+    int b = max(abs(x1 - x2), abs(y1 - y2));
+    return gcd(a, b) - 1;
+}
