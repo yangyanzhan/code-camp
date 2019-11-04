488a5b103469d8027a342a92e680357557e8305a
yangyanzhan <yangyanzhan@gmail.com>
2019-11-04 11:28:39 +0800

    add Yanzhan's cpp solution for the filling jars problem on hackerrank

---
+++ hackerrank/c++/filling-jars.cpp
@@ -0,0 +1,12 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+unsigned long long solve(int n, vector<vector<int>> operations) {
+    unsigned long long total = 0;
+    for (auto operation: operations) {
+        unsigned long long a = operation[0], b = operation[1], c = operation[2];
+        total += (b - a + 1) * c;
+    }
+    return total / n;
+}
