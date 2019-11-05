840b44c790afcf2f76f47e67a7b8f11edd856e19
yangyanzhan <yangyanzhan@gmail.com>
2019-11-06 00:18:08 +0800

    add Yanzhan's cpp solution for the grading students problem on hackerrank

---
+++ hackerrank/c++/grading-students.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+vector<int> gradingStudents(vector<int> grades) {
+    vector<int> res;
+    for (auto grade: grades) {
+        if (grade < 38) {
+        } else if (grade % 5 != 0) {
+            int target = (grade / 5 + 1) * 5;
+            if (target - grade < 3) {
+                grade = target;
+            }
+        }
+        res.push_back(grade);
+    }
+    return res;
+}
