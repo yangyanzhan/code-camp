5cc2311a2b4f2186fa3e33c660c62829b41c69fd
yangyanzhan <yangyanzhan@gmail.com>
2019-11-05 12:03:40 +0800

    add Yanzhan's cpp solution for the identify smith numbers problem on hackerrank

---
+++ hackerrank/c++/identify-smith-numbers.cpp
@@ -0,0 +1,34 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int sum(int n) {
+    int res = 0;
+    while (n >= 10) {
+        res += n % 10;
+        n /= 10;
+    }
+    res += n;
+    return res;
+}
+
+vector<int> factors(int n) {
+    vector<int> res;
+    int factor = 1;
+    while (n >= ++factor) {
+        while (n % factor == 0) {
+            res.push_back(factor);
+            n /= factor;
+        }
+    }
+    return res;
+}
+
+int solve(int n) {
+    int sum1 = sum(n);
+    int sum2 = 0;
+    for (auto factor: factors(n)) {
+        sum2 += sum(factor);
+    }
+    return sum1 == sum2 ? 1 : 0;
+}
