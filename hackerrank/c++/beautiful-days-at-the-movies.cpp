d1a5faeb1f9ab213cc5adef2b6c1f6c90d06f615
yangyanzhan <yangyanzhan@gmail.com>
2019-11-06 17:14:46 +0800

    add Yanzhan's cpp solution for the beautiful days at the movies problem on hackerrank

---
+++ hackerrank/c++/beautiful-days-at-the-movies.cpp
@@ -0,0 +1,23 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int reversed(int n) {
+    int res = 0;
+    while (n > 0) {
+        res *= 10;
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+int beautifulDays(int i, int j, int k) {
+    int res = 0;
+    for (int n = i; n <= j; n++) {
+        if (abs(n - reversed(n)) % k == 0) {
+            res++;
+        }
+    }
+    return res;
+}
