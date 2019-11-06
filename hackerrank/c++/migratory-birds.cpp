7054fc5d7acfe9471b4f299f2d89b412da1ed56f
yangyanzhan <yangyanzhan@gmail.com>
2019-11-06 14:52:48 +0800

    add Yanzhan's cpp solution for the migratory birds problem on hackerrank

---
+++ hackerrank/c++/migratory-birds.cpp
@@ -0,0 +1,21 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int migratoryBirds(vector<int> arr) {
+    map<int, int> cache;
+    for (auto num: arr) {
+        if (cache.find(num) == cache.end()) {
+            cache[num] = 0;
+        }
+        cache[num]++;
+    }
+    int max_count = 0, max_bird = 0;
+    for (auto it: cache) {
+        if (it.second > max_count) {
+            max_count = it.second;
+            max_bird = it.first;
+        }
+    }
+    return max_bird;
+}
