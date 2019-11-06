4f817a771ec8b092ec22ff2a3e3d16386c0a8bad
yangyanzhan <yangyanzhan@gmail.com>
2019-11-06 16:30:23 +0800

    add Yanzhan's cpp solution for the electronics shop problem on hackerrank

---
+++ hackerrank/c++/electronics-shop.cpp
@@ -0,0 +1,16 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int getMoneySpent(vector<int> keyboards, vector<int> drives, int b) {
+    int max_sum = -1;
+    for (auto keyboard: keyboards) {
+        for (auto drive: drives) {
+            long long sum = (long long)keyboard + drive;
+            if (sum <= b) {
+                max_sum = max(max_sum, (int)sum);
+            }
+        }
+    }
+    return max_sum;
+}
