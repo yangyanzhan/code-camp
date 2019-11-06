bc9a06fe897abf13fff20cbb3ec7ee1ed22c6ce0
yangyanzhan <yangyanzhan@gmail.com>
2019-11-06 17:06:43 +0800

    add Yanzhan's cpp solution for the designer pdf viewer problem on hackerrank

---
+++ hackerrank/c++/designer-pdf-viewer.cpp
@@ -0,0 +1,11 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int designerPdfViewer(vector<int> h, string word) {
+    int max_h = 0;
+    for (auto ch: word) {
+        max_h = max(max_h, h[ch - 'a']);
+    }
+    return max_h * word.size();
+}
