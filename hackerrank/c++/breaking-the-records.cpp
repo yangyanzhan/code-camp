1c4df5824e3e276610bc5bf438819cee5726839c
yangyanzhan <yangyanzhan@gmail.com>
2019-11-06 13:04:14 +0800

    add Yanzhan's cpp solution for the breaking the records problem on hackerrank

---
+++ hackerrank/c++/breaking-the-records.cpp
@@ -0,0 +1,19 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+vector<int> breakingRecords(vector<int> scores) {
+    int max_score = scores[0], min_score = scores[0], max_count = 0, min_count = 0;
+    for (int i = 1; i < scores.size(); i++) {
+        int score = scores[i];
+        if (score > max_score) {
+            max_score = score;
+            max_count++;
+        }
+        if (score < min_score) {
+            min_score = score;
+            min_count++;
+        }
+    }
+    return {max_count, min_count};
+}
