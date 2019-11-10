eb99a66b7b0777a05fc4b925bc1149790a53fab7
yangyanzhan <yangyanzhan@gmail.com>
2019-11-10 20:57:41 +0800

    add Yanzhan's cpp solution for the strong password problem on hackerrank

---
+++ hackerrank/c++/strong-password.cpp
@@ -0,0 +1,33 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+bool has_char(string word, string pool) {
+    for (auto ch: word) {
+        if (pool.find(ch) != string::npos) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int minimumNumber(int n, string password) {
+    int m = password.size(), res = 0;
+    if (!has_char(password, "0123456789")) {
+        res++;
+    }
+    if (!has_char(password, "abcdefghijklmnopqrstuvwxyz")) {
+        res++;
+    }
+    if (!has_char(password, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")) {
+        res++;
+    }
+    if (!has_char(password, "!@#$%^&*()-+")) {
+        res++;
+    }
+    m += res;
+    if (m < 6) {
+        res += 6 - m;
+    }
+    return res;
+}
