647313640e1732d3306edb0fd1319d8fcb7c2086
yangyanzhan <yangyanzhan@gmail.com>
2019-11-10 17:24:34 +0800

    add Yanzhan's youtube and twitter homepage to leetcode python solutions

--- leetcode/python/16.py
+++ leetcode/python/16.py
@@ -4,6 +4,8 @@ __author__ = "yangyanzhan"
 __email__ = "yangyanzhan@gmail.com"
 __url__ = "https://github.com/yangyanzhan/code-camp"
 __blog__ = "https://yangyanzhan.github.io"
+__youtube__ = "https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber"
+__twitter__ = "https://twitter.com/YangYanzhan"
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
