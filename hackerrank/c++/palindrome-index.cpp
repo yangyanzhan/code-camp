7abf94e4d31e086c0de6dac647e1305f61f74f35
yangyanzhan <yangyanzhan@gmail.com>
2019-11-12 16:16:20 +0800

    add Yanzhan's cpp solution for the palindrome index problem on hackerrank

---
+++ hackerrank/c++/palindrome-index.cpp
@@ -0,0 +1,39 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int palindromeIndex(string s) {
+    int begin = 0, end = s.size() - 1;
+    while (begin < end) {
+        if (s[begin] != s[end]) {
+            int ori_begin = begin, ori_end =end;
+            begin++;
+            while (begin < end) {
+                if (s[begin] != s[end]) {
+                    break;
+                }
+                begin++;
+                end--;
+            }
+            if (begin >= end) {
+                return ori_begin;
+            }
+            begin = ori_begin;
+            end = ori_end - 1;
+            while (begin < end) {
+                if (s[begin] != s[end]) {
+                    break;
+                }
+                begin++;
+                end--;
+            }
+            if (begin >= end) {
+                return ori_end;
+            }
+            break;
+        }
+        begin++;
+        end--;
+    }
+    return -1;
+}
