20c2deac498ba71a0b345bf6f531c0988e97d27d
yangyanzhan <yangyanzhan@gmail.com>
2019-11-12 11:28:24 +0800

    add Yanzhan's cpp solution for the correctness and the loop invariant problem on hackerrank

---
+++ hackerrank/c++/correctness-and-the-loop-invariant.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+void insertionSort(int N, int arr[]) {
+    int i,j;
+    int value;
+    for(i=1;i<N;i++)
+    {
+        value=arr[i];
+        j=i-1;
+        while(j>=0 && value<arr[j])
+        {
+            arr[j+1]=arr[j];
+            j=j-1;
+        }
+        arr[j+1]=value;
+    }
+    for(j=0;j<N;j++)
+    {
+        printf("%d",arr[j]);
+        printf(" ");
+    }
+}
