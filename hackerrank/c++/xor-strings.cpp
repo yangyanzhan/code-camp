cccbe44297296d9fc468689600a3f60076311cca
yangyanzhan <yangyanzhan@gmail.com>
2019-11-12 17:39:19 +0800

    add Yanzhan's cpp solution for the xor strings problem on hackerrank

---
+++ hackerrank/c++/xor-strings.cpp
@@ -0,0 +1,16 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+string strings_xor(string s, string t) {
+
+    string res = "";
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == t[i])
+            res += "0";
+        else
+            res += "1";
+    }
+
+    return res;
+}
