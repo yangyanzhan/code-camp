8b575acffb5ba9feb6f294347239eab7ed30c0be
yangyanzhan <yangyanzhan@gmail.com>
2019-11-12 16:25:25 +0800

    add Yanzhqn's cpp solution for the making anagrams problem on hackerrank

---
+++ hackerrank/c++/making-anagrams.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+
+int makingAnagrams(string s1, string s2) {
+    map<char, int> cache;
+    for (auto ch: s1) {
+        if (cache.find(ch) == cache.end()) {
+            cache[ch] = 0;
+        }
+        cache[ch]++;
+    }
+    for (auto ch: s2) {
+        if (cache.find(ch) == cache.end()) {
+            cache[ch] = 0;
+        }
+        cache[ch]--;
+    }
+    int res = 0;
+    for (auto it: cache) {
+        res += abs(it.second);
+    }
+    return res;
+}
