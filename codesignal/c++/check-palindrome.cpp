4da65bb7a9ed9103f3ee0d9e0b907a3eb8c1c0cd
yangyanzhan <yangyanzhan@gmail.com>
2019-11-17 14:44:15 +0800

    add Yanzhan's cpp solution for the check palindrome problem on codesignal

---
+++ codesignal/c++/check-palindrome.cpp
@@ -0,0 +1,10 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/check-palindrome.html .
+
+bool checkPalindrome(std::string inputString) {
+    std::string s1 = inputString, s2 = inputString;
+    reverse(s2.begin(), s2.end());
+    return s1 == s2;
+}
