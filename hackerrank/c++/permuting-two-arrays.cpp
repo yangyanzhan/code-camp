34e2563d897739b9b3f53527eb179882193f8b6a
yangyanzhan <yangyanzhan@gmail.com>
2019-11-18 00:18:00 +0800

    add Yanzhan's cpp solution for the permuting two arrays problem on hackerrank

---
+++ hackerrank/c++/permuting-two-arrays.cpp
@@ -0,0 +1,16 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/hackerrank/permuting-two-arrays.html .
+
+string twoArrays(int k, vector<int> A, vector<int> B) {
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    reverse(B.begin(), B.end());
+    for (int i = 0; i < A.size(); i++) {
+        if (A[i] + B[i] < k) {
+            return "NO";
+        }
+    }
+    return "YES";
+}
