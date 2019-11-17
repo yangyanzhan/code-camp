1ba133104d93db1d859c7d86d6e6ee1fe206ff3d
yangyanzhan <yangyanzhan@gmail.com>
2019-11-17 14:10:42 +0800

    add Yanzhan's cpp solution for the jim and the orders problem on hackerrank

---
+++ hackerrank/c++/jim-and-the-orders.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/hackerrank/.html .
+
+vector<int> jimOrders(vector<vector<int>> orders) {
+    int n = orders.size();
+    vector<tuple<int, int>> items;
+    vector<int> res;
+    for (int i = 0; i < n; i++) {
+        items.push_back(make_tuple(orders[i][0] + orders[i][1], i + 1));
+    }
+    sort(items.begin(), items.end());
+    for (int i = 0; i < n; i++) {
+        res.push_back(get<1>(items[i]));
+    }
+    return res;
+}
