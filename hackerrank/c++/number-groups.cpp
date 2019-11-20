1e52a9aa20d04676abf6ce5dfd4022526c5b283e
yangyanzhan <yangyanzhan@gmail.com>
2019-11-20 15:29:52 +0800

    add Yanzhan's cpp solution for the number groups problem on hackerrank

---
+++ hackerrank/c++/number-groups.cpp
@@ -0,0 +1,9 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/hackerrank/number-groups.html .
+
+long sumOfGroup(int k) {
+    long long m = (long long)k * (k - 1) / 2, n = 2 * m + 1;
+    return (n + n + 2 * (k - 1)) * k / 2;
+}
