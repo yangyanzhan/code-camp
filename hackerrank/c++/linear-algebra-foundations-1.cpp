21779bf189ece09b9cf34572c867d5275d1e20c1
yangyanzhan <yangyanzhan@gmail.com>
2019-11-21 23:26:27 +0800

    add Yanzhan's cpp solution for the linear algebra foundations problem on hackerrank

---
+++ hackerrank/c++/linear-algebra-foundations-1.cpp
@@ -0,0 +1,14 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/hackerrank/linear-algebra-foundations-1.html .
+
+5
+7
+9
+9
+11
+13
+5
+6
+8
