2f2651e1b75a1d850cb6039965ce3d14a434cf55
yangyanzhan <yangyanzhan@gmail.com>
2019-11-22 12:40:46 +0800

    add Yanzhan's cpp solution for the a chessboard game problem on hackerrank

---
+++ hackerrank/c++/a-chessboard-game.cpp
@@ -0,0 +1,13 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/hackerrank/a-chessboard-game.html .
+
+string chessboardGame(int x, int y) {
+    x %= 4;
+    y %= 4;
+    if (y == 0 || y == 3 || x == 0 || x == 3) {
+        return "First";
+    }
+    return "Second";
+}
