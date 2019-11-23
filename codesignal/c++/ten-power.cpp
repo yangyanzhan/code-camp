4e033225bd7897170feea2c96cc62cee45667090
yangyanzhan <yangyanzhan@gmail.com>
2019-11-24 01:11:22 +0800

    add Yanzhan's cpp solution for the ten power problem on codesignal

---
+++ codesignal/c++/ten-power.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/ten-power.html .
+
+int tens(int n) {
+    return (n % 100) / 10;
+}
+
+int TenPower(std::vector<int> numbers) {
+    int res = 0, count = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+        int n = numbers[i];
+        if (n >= 10) {
+            if (count % 2 == 0) {
+                res += tens(n);
+            } else {
+                res -= tens(n);
+            }
+            count++;
+        }
+    }
+    return res;
+}
