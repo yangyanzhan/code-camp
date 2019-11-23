295aecd1359367c2b21cb04981c048c51f3cc2d4
yangyanzhan <yangyanzhan@gmail.com>
2019-11-23 21:09:49 +0800

    add Yanzhan's cpp solution for the make sentence problem on codesignal

---
+++ codesignal/c++/make-sentence.cpp
@@ -0,0 +1,17 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/make-sentence.html .
+
+std::string makeSentence(std::vector<std::vector<char>> words) {
+    string res;
+    for (int i = 0; i < words.size(); i++) {
+        for (int j = 0; j < words[i].size(); j++) {
+            res += words[i][j];
+        }
+        res += " ";
+    }
+    res = res.substr(0, res.length() - 1);
+    res += ".";
+    return res;
+}
