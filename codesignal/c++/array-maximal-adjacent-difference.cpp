bd6b96d2839a6f6507b4cd6d872848cd2bd4a560
yangyanzhan <yangyanzhan@gmail.com>
2019-11-24 22:20:47 +0800

    add Yanzhan's cpp solution for the array maximal adjacent difference problem on codesignal

---
+++ codesignal/c++/array-maximal-adjacent-difference.cpp
@@ -0,0 +1,13 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/array-maximal-adjacent-difference.html .
+
+int arrayMaximalAdjacentDifference(std::vector<int> inputArray) {
+    using namespace std;
+    int res = 0;
+    for (int i = 1; i < inputArray.size(); i++) {
+        res = max(res, abs(inputArray[i] - inputArray[i - 1]));
+    }
+    return res;
+}
