122a1e0e6a18bb4335e4798356df66042d5db122
yangyanzhan <yangyanzhan@gmail.com>
2019-11-24 14:41:32 +0800

    add Yanzhan's cpp solution for the modulo harshad problem on codesignal

---
+++ codesignal/c++/modulo-harshad.cpp
@@ -0,0 +1,31 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/modulo-harshad.html .
+
+bool isHarshad(long long num) {
+    using namespace std;
+    int sum = 0;
+    string item = to_string(num);
+    for (int i = 0; i < item.size(); i++) {
+        sum += item[i] - '0';
+    }
+    return num % sum == 0;
+}
+
+long long moduloHarshad(long long num1, long long num2) {
+    int c = 0, a = 0, b = 0, num = 0;
+    while (c < max(num1, num2)) {
+        num++;
+        if (isHarshad(num)) {
+            c++;
+            if (c == num1) {
+                a = num;
+            }
+            if (c == num2) {
+                b = num;
+            }
+        }
+    }
+    return b % a;
+}
