b38bd9929ad2eef2e3ffbab0ca09fad001886a74
yangyanzhan <yangyanzhan@gmail.com>
2019-11-24 11:54:34 +0800

    add Yanzhan's cpp solution for the random 1 problem on codesignal

---
+++ codesignal/c++/random-1.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/random-1.html .
+
+int random1(int seed, int n) {
+    using namespace std;
+    long long res = seed;
+    int len = to_string(res).size();
+    for (int i = 1; i <= n; i++) {
+        res *= res;
+        string whole = to_string(res);
+        while (2 * len != whole.size()) {
+            whole = "0" + whole;
+        }
+        string part = whole.substr(len / 2, len);
+        res = stoll(part);
+    }
+    return res;
+}
