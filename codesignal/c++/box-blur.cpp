763e53b495fdeeb0201bffec441bbe61ff207676
yangyanzhan <yangyanzhan@gmail.com>
2019-11-24 22:51:12 +0800

    add Yanzhan's cpp solution for the box blur problem on codesignal

---
+++ codesignal/c++/box-blur.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/box-blur.html .
+
+std::vector<std::vector<int>> boxBlur(std::vector<std::vector<int>> image) {
+    using namespace std;
+    int m = image.size(), n = image[0].size();
+    vector<vector<int>> res;
+    for (int i = 1; i < m - 1; i++) {
+        vector<int> row;
+        for (int j = 1; j < n - 1; j++) {
+            int sum = 0;
+            for (int i1 = -1; i1 <= 1; i1++) {
+                for (int j1 = -1; j1 <= 1; j1++) {
+                    sum += image[i + i1][j + j1];
+                }
+            }
+            row.push_back(sum / 9);
+        }
+        res.push_back(row);
+    }
+    return res;
+}
