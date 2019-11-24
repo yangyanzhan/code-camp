0fdc6124f3cbd6d3f6672647d2c6449efd2702ce
yangyanzhan <yangyanzhan@gmail.com>
2019-11-24 22:36:45 +0800

    add Yanzhan's cpp solution for the is ipv4 address problem on codesignal

---
+++ codesignal/c++/is-ipv4-address.cpp
@@ -0,0 +1,32 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/is-ipv4-address.html .
+
+#include <regex>
+
+bool isIPv4Address(std::string inputString) {
+    using namespace std;
+    regex reg("^(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)$");
+    smatch matches;
+    regex_search(inputString, matches, reg);
+    if (matches.size() != 5) {
+        return false;
+    }
+    for (int i = 1; i <= 4; i++) {
+        string item = matches[i].str();
+        if (item == "0") {
+            continue;
+        }
+        if (item[0] == '0') {
+            return false;
+        }
+        if (item.size() > 3) {
+            return false;
+        }
+        if (stoi(item) > 255) {
+            return false;
+        }
+    }
+    return true;
+}
