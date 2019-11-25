e5929046992b25edbd8643e026ae59f2e6bde374
yangyanzhan <yangyanzhan@gmail.com>
2019-11-26 00:08:49 +0800

    add Yanzhan's cpp solution for the valid time problem on codesignal

---
+++ codesignal/c++/valid-time.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/valid-time.html .
+
+#include <regex>
+
+bool validTime(std::string time) {
+    using namespace std;
+    regex reg("^(\\d\\d):(\\d\\d)$");
+    smatch matches;
+    regex_search(time, matches, reg);
+    if (matches.size() != 3) {
+        return false;
+    }
+    int h = stoi(matches[1].str());
+    int m = stoi(matches[2].str());
+    return 0 <= h && h <= 23 && 0 <= m && m <= 59;
+}
+
