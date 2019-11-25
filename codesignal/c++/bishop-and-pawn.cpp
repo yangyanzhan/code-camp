6036ad88b0f977040283d850676bf41499b75157
yangyanzhan <yangyanzhan@gmail.com>
2019-11-25 17:14:44 +0800

    add Yanzhan's cpp solution for the bishop and pawn problem on codesignal

---
+++ codesignal/c++/bishop-and-pawn.cpp
@@ -0,0 +1,9 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/bishop-and-pawn.html .
+
+bool bishopAndPawn(std::string bishop, std::string pawn) {
+    using namespace std;
+    return abs(bishop[0] - pawn[0]) == abs(bishop[1] - pawn[1]);
+}
