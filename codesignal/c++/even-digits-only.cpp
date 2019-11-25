f0239235715e8941774f156d154b7948781c7085
yangyanzhan <yangyanzhan@gmail.com>
2019-11-25 12:44:22 +0800

    add Yanzhan's cpp solution for the even digits only problem on codesignal

---
+++ codesignal/c++/even-digits-only.cpp
@@ -0,0 +1,15 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/even-digits-only.html .
+
+bool evenDigitsOnly(int n) {
+    while (n > 0) {
+        int digit = n % 10;
+        n /= 10;
+        if (digit % 2 != 0) {
+            return false;
+        }
+    }
+    return true;
+}
