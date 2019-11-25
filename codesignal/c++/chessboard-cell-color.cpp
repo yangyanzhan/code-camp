c12086e11cd76a0fce22dc7e06ff6ae4b412be66
yangyanzhan <yangyanzhan@gmail.com>
2019-11-25 14:10:57 +0800

    add Yanzhan's cpp solution for the chessboard cell color problem on codesignal

---
+++ codesignal/c++/chessboard-cell-color.cpp
@@ -0,0 +1,9 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/chessboard-cell-color.html .
+
+bool chessBoardCellColor(std::string cell1, std::string cell2) {
+    using namespace std;
+    return (cell1[0] - cell2[0] + cell1[1] - cell2[1]) % 2 == 0;
+}
