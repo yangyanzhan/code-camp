0b10ac0ac556f3ac5c4a2c947dde03b1c3566958
yangyanzhan <yangyanzhan@gmail.com>
2019-11-25 15:41:37 +0800

    add Yanzhan's cpp solution for the extract each kth problem on codesignal

---
+++ codesignal/c++/extract-each-kth.cpp
@@ -0,0 +1,15 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/extract-each-kth.html .
+
+std::vector<int> extractEachKth(std::vector<int> inputArray, int k) {
+    using namespace std;
+    vector<int> res;
+    for (int i = 0; i < inputArray.size(); i++) {
+        if ((i + 1) % k != 0) {
+            res.push_back(inputArray[i]);
+        }
+    }
+    return res;
+}
