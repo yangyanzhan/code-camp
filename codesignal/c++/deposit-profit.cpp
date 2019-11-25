3b1caef45e8320a9b4c591edef9dcf22ba07efad
yangyanzhan <yangyanzhan@gmail.com>
2019-11-25 14:23:58 +0800

    add Yanzhan's cpp solution for the deposit profit problem on codesignal

---
+++ codesignal/c++/deposit-profit.cpp
@@ -0,0 +1,8 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/deposit-profit.html .
+
+int depositProfit(int deposit, int rate, int threshold) {
+    return ceil(log((threshold + 0.0) / deposit) / log((100 + rate) / 100.0));
+}
