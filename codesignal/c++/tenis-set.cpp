5438c3abe6a047b90b1a5b3550913a41528964d9
yangyanzhan <yangyanzhan@gmail.com>
2019-11-26 14:24:53 +0800

    add Yanzhan's cpp solution for the tenis set problem on codesignal

---
+++ codesignal/c++/tenis-set.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/tenis-set.html .
+
+bool tennisSet(int score1, int score2) {
+    int s1, s2;
+    if (score1 >= score2) {
+        s1 = score1;
+        s2 = score2;
+    } else {
+        s1 = score2;
+        s2 = score1;
+    }
+    if (s1 == 7) {
+        return 5 <= s2 && s2 < 7;
+    }
+    return s1 == 6 && s2 < 5;
+}
+
