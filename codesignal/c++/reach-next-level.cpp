53a2a61940964b181f50d5b3878bd26ddf36441f
yangyanzhan <yangyanzhan@gmail.com>
2019-11-26 14:10:55 +0800

    add Yanzhan's cpp solution for the reach next level problem on codesignal

---
+++ codesignal/c++/reach-next-level.cpp
@@ -0,0 +1,9 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/reach-next-level.html .
+
+bool reachNextLevel(int experience, int threshold, int reward) {
+    return experience + reward >= threshold;
+}
+
