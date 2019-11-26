38ced77c7b01957ee9e2349a295670ab7c041409
yangyanzhan <yangyanzhan@gmail.com>
2019-11-26 15:21:17 +0800

    add Yanzhan's cpp solution for the mirror bits problem on codesignal

---
+++ codesignal/c++/mirror-bits.cpp
@@ -0,0 +1,14 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/mirror-bits.html .
+
+int mirrorBits(int a) {
+    int res = 0;
+    while (a > 0) {
+        res = res * 2 + a % 2;
+        a /= 2;
+    }
+    return res;
+}
+
