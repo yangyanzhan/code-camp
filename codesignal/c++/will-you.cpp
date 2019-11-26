ec8e915a5125ef7da0d6a6935f39d3dad62afc7b
yangyanzhan <yangyanzhan@gmail.com>
2019-11-26 14:29:12 +0800

    add Yanzhan's cpp solution for the will you problem on codesignal

---
+++ codesignal/c++/will-you.cpp
@@ -0,0 +1,9 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/will-you.html .
+
+bool willYou(bool young, bool beautiful, bool loved) {
+    return (young && beautiful && !loved) || (loved && !(young && beautiful));
+}
+
