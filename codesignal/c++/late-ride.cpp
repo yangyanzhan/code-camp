b256609d112c5e9da3ede58f494bbad39dae4fda
yangyanzhan <yangyanzhan@gmail.com>
2019-11-26 13:09:29 +0800

    add Yanzhan's cpp solution for the late ride problem on codesignal

---
+++ codesignal/c++/late-ride.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/late-ride.html .
+
+int sum(int n) {
+    int res = 0;
+    while (n > 0) {
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+int lateRide(int n) {
+    return sum(n / 60) + sum(n % 60);
+}
+
