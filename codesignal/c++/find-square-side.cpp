77ebb1beff6407c8a2855209a95590bc3baf7711
yangyanzhan <yangyanzhan@gmail.com>
2019-11-28 15:17:24 +0800

    add Yanzhan's cpp solution for the find square side problem on codesignal

---
+++ codesignal/c++/find-square-side.cpp
@@ -0,0 +1,28 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/find-square-side.html .
+
+int findSquareSide(std::vector<int> x, std::vector<int> y) {
+    using namespace std;
+    int res = 0, count = 0;
+    for (int i = 0; i < x.size(); i++) {
+        for (int j = i + 1; j < x.size(); j++) {
+            int dx = x[i] - x[j], dy = y[i] - y[j];
+            double dist = sqrt(dx * dx + dy * dy);
+            int size = dist * dist;
+            if (count == 0) {
+                res = size;
+                count++;
+            } else {
+                if (res == size) {
+                    count++;
+                } else {
+                    count--;
+                }
+            }
+        }
+    }
+    return res;
+}
+
