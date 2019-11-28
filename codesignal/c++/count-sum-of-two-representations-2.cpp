50d069bc0c8913232a778fab804fed238079b6bd
yangyanzhan <yangyanzhan@gmail.com>
2019-11-28 16:31:04 +0800

    add Yanzhan's cpp solution for the count sum of two representations 2 problem on codesignal

---
+++ codesignal/c++/count-sum-of-two-representations-2.cpp
@@ -0,0 +1,9 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/count-sum-of-two-representations-2.html .
+
+int countSumOfTwoRepresentations2(int n, int l, int r) {
+    int a1 = max(l, n - r), a2 = min(r, n / 2);
+    return max(0, a2 - a1 + 1);
+}
