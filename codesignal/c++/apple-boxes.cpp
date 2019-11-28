b10c0f06e0d6c0fa5226786201b97722b4ba6920
yangyanzhan <yangyanzhan@gmail.com>
2019-11-28 16:55:45 +0800

    add Yanzhan's cpp solution for the apple boxes problem on codesignal

---
+++ codesignal/c++/apple-boxes.cpp
@@ -0,0 +1,17 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/apple-boxes.html .
+
+int appleBoxes(int k) {
+    int a = 0, b = 0;
+    for (int i = 1; i <= k; i++) {
+        if (i % 2 == 0) {
+            b += i * i;
+        } else {
+            a += i * i;
+        }
+    }
+    return b - a;
+}
+
