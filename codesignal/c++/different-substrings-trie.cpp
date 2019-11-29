19728e79fc48e323e0d3f29a85b369ea9b60cc27
yangyanzhan <yangyanzhan@gmail.com>
2019-11-29 16:48:00 +0800

    add Yanzhan's cpp solution for the different substrings trie problem on codesignal

---
+++ codesignal/c++/different-substrings-trie.cpp
@@ -0,0 +1,16 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/different-substrings-trie.html .
+
+int differentSubstringsTrie(std::string inputString) {
+    using namespace std;
+    set<string> cache;
+    for (int len = 1; len <= inputString.size(); len++) {
+        for (int i = 0; i + len <= inputString.size(); i++) {
+            cache.insert(inputString.substr(i, len));
+        }
+    }
+    return cache.size();
+}
+
