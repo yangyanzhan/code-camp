37357a874b0b562705190ecd2ba66b54de12e1d6
yangyanzhan <yangyanzhan@gmail.com>
2019-12-01 23:37:13 +0800

    add Yanzhan's cpp solution for the concatenate arrays problem on codesignal

---
+++ codesignal/c++/concatenate-arrays.cpp
@@ -0,0 +1,10 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/concatenate-arrays.html .
+
+std::vector<int> concatenateArrays(std::vector<int> a, std::vector<int> b) {
+    a.insert(a.end(), b.begin(), b.end());
+    return a;
+}
+
