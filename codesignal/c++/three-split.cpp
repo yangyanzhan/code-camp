525906b0c634dda65d891497b7be184208751b72
yangyanzhan <yangyanzhan@gmail.com>
2019-12-02 15:56:40 +0800

    add Yanzhan's cpp solution for the three split problem on codesignal

---
+++ codesignal/c++/three-split.cpp
@@ -0,0 +1,30 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/three-split.html .
+
+int threeSplit(std::vector<int> nums) {
+    long long sum = 0;
+    for (auto num: nums) {
+        sum += num;
+    }
+    sum /= 3;
+    int res = 0;
+    long long s1 = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        s1 += nums[i];
+        if (s1 == sum) {
+            long long s2 = 0;
+            for (int j = i + 1; j < nums.size(); j++) {
+                s2 += nums[j];
+                if (s2 == sum) {
+                    if (j < nums.size() - 1) {
+                        res++;
+                    }
+                }
+            }
+        }
+    }
+    return res;
+}
+
