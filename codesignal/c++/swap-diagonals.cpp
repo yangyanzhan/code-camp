b61b4951de8d80b36159e762fd3b246009be440d
yangyanzhan <yangyanzhan@gmail.com>
2019-12-02 23:13:34 +0800

    add Yanzhan's cpp solution for the swap diagonals problem on codesignal

---
+++ codesignal/c++/swap-diagonals.cpp
@@ -0,0 +1,14 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/swap-diagonals.html .
+
+std::vector<std::vector<int>> swapDiagonals(std::vector<std::vector<int>> matrix) {
+    using namespace std;
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+        swap(matrix[i][i], matrix[i][n - 1 - i]);
+    }
+    return matrix;
+}
+
