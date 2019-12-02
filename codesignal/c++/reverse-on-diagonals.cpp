cd9545108580d58bc6bb4f7e1b3274991a85cf78
yangyanzhan <yangyanzhan@gmail.com>
2019-12-02 23:10:41 +0800

    add Yanzhan's cpp solution for the reverse on diagonals problem on codesignal

---
+++ codesignal/c++/reverse-on-diagonals.cpp
@@ -0,0 +1,15 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/reverse-on-diagonals.html .
+
+std::vector<std::vector<int>> reverseOnDiagonals(std::vector<std::vector<int>> matrix) {
+    using namespace std;
+    int n = matrix.size();
+    for (int i = 0; i < n / 2; i++) {
+        swap(matrix[i][i], matrix[n - 1 - i][n - 1 - i]);
+        swap(matrix[n - 1 - i][i], matrix[i][n - 1 - i]);
+    }
+    return matrix;
+}
+
