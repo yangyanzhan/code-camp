230a78d77fb4f3eb2d114872d8ef780222800274
yangyanzhan <yangyanzhan@gmail.com>
2019-12-02 14:27:14 +0800

    add Yanzhan's cpp solution for the most frequent digit sum problem on codesignal

---
+++ codesignal/c++/most-frequent-digit-sum.cpp
@@ -0,0 +1,36 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/most-frequent-digit-sum.html .
+
+int x(int n) {
+    int res = 0;
+    while (n > 0) {
+        res += (n % 10);
+        n /= 10;
+    }
+    return res;
+}
+
+int mostFrequentDigitSum(int n) {
+    map<int, int> mapping;
+    mapping[0] = 1;
+    while (n != 0) {
+        int dec = x(n);
+        if (mapping.find(dec) == mapping.end()) {
+            mapping[dec] = 0;
+        }
+        mapping[dec]++;
+        n -= dec;
+    }
+    int best, bestCount = 0;
+    for (auto pair: mapping) {
+        int key = pair.first, count = pair.second;
+        if (count >= bestCount) {
+            bestCount = count;
+            best = key;
+        }
+    }
+    return best;
+}
+
