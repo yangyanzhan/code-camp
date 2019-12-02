8be8181386af5da502cd9d721cead4d8ce738a87
yangyanzhan <yangyanzhan@gmail.com>
2019-12-02 21:20:35 +0800

    add Yanzhan's cpp solution for the pair of shoes problem on codesignal

---
+++ codesignal/c++/pair-of-shoes.cpp
@@ -0,0 +1,27 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/pair-of-shoes.html .
+
+bool pairOfShoes(std::vector<std::vector<int>> shoes) {
+    using namespace std;
+    map<int, int> cache;
+    for (auto pair: shoes) {
+        int type = pair[0], size = pair[1];
+        if (cache.find(size) == cache.end()) {
+            cache[size] = 0;
+        }
+        if (type == 0) {
+            cache[size]--;
+        } else {
+            cache[size]++;
+        }
+    }
+    for (auto pair: cache) {
+        if (pair.second != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
