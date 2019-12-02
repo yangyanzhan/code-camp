0ccb2e8b61b2145afc632630a7c41506130b6f9e
yangyanzhan <yangyanzhan@gmail.com>
2019-12-02 15:13:56 +0800

    add Yanzhan's cpp solution for the timed reading problem on codesignal

---
+++ codesignal/c++/timed-reading.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/timed-reading.html .
+
+int timedReading(int maxLength, std::string text) {
+    using namespace std;
+    int res = 0;
+    string word = "";
+    text += ".";
+    for (auto ch: text) {
+        ch = tolower(ch);
+        if (ch < 'a' || ch > 'z') {
+            if (word.size() > 0 && word.size() <= maxLength) {
+                res++;
+            }
+            word = "";
+        } else {
+            word += ch;
+        }
+    }
+    return res;
+}
+
