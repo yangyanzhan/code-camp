a4806aa6af4bde4b08e2d55b4984223238ffac59
yangyanzhan <yangyanzhan@gmail.com>
2019-12-02 14:45:05 +0800

    add Yanzhan's cpp solution for the house of cats problem on codesignal

---
+++ codesignal/c++/house-of-cats.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/house-of-cats.html .
+
+std::vector<int> houseOfCats(int legs) {
+    using namespace std;
+    vector<int> res;
+    int num = 0;
+    while (2 * num <= legs) {
+        if ((legs - 2 * num) % 4 == 0) {
+            res.push_back(num);
+        }
+        num++;
+    }
+    return res;
+}
+
