d54486c9990491a4cb2b0cfe0964978ef09373b4
yangyanzhan <yangyanzhan@gmail.com>
2019-12-02 21:56:33 +0800

    add Yanzhan's cpp solution for the beautiful text problem on codesignal

---
+++ codesignal/c++/beautiful-text.cpp
@@ -0,0 +1,26 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/beautiful-text.html .
+
+bool beautifulText(std::string inputString, int l, int r) {
+    for (int n = l; n <= r; n++) {
+        if (inputString.size() % (n + 1) == 0) {
+        } else if ((inputString.size() + 1) % (n + 1) == 0) {
+        } else {
+            continue;
+        }
+        bool valid = true;
+        for (int i = 0; i < inputString.size() / (n + 1); i++) {
+            if (inputString[(i + 1) * (n + 1) - 1] != ' ') {
+                valid = false;
+                break;
+            }
+        }
+        if (valid) {
+            return true;
+        }
+    }
+    return false;
+}
+
