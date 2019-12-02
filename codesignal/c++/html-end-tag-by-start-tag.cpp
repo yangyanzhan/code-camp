857ffbeed1c51cb691fb6d5b2ee0cefb9da3f489
yangyanzhan <yangyanzhan@gmail.com>
2019-12-02 11:51:08 +0800

    add Yanzhan's cpp solution for the html end tag by start tag problem on codesignal

---
+++ codesignal/c++/html-end-tag-by-start-tag.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/html-end-tag-by-start-tag.html .
+
+std::string htmlEndTagByStartTag(std::string startTag) {
+    using namespace std;
+    int pos = 0;
+    while (++pos < startTag.length()) {
+        if (startTag[pos] == '>' || startTag[pos] == ' ') {
+            break;
+        }
+    }
+    string res = startTag.substr(0, pos) + ">";
+    res.insert(1, "/");
+    return res;
+}
+
