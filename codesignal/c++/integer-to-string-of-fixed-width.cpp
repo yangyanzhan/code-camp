c0ac1b61cc4a92c1a1b8d51108c0f83d15e37d93
yangyanzhan <yangyanzhan@gmail.com>
2019-12-02 15:21:22 +0800

    add Yanzhan's cpp solution for the integer to string of fixed width problem on codesignal

---
+++ codesignal/c++/integer-to-string-of-fixed-width.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/integer-to-string-of-fixed-width.html .
+
+std::string integerToStringOfFixedWidth(int number, int width) {
+    using namespace std;
+    string res = to_string(number);
+    if (width <= res.size()) {
+        return res.substr(res.size() - width);
+    } else {
+        while (res.size() < width) {
+            res = "0" + res;
+        }
+        return res;
+    }
+}
+
