df66b3708cd3cdd0dbfdec1facc554a3c8696b4e
yangyanzhan <yangyanzhan@gmail.com>
2019-12-03 12:47:57 +0800

    add Yanzhan's cpp solution for the digit distance number problem on codesignal

---
+++ codesignal/c++/digit-distance-number.cpp
@@ -0,0 +1,15 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/digit-distance-number.html .
+
+int digitDistanceNumber(int n) {
+    using namespace std;
+    string item1 = to_string(n);
+    string item2 = "";
+    for (int i = 1; i < item1.size(); i++) {
+        item2 += to_string(abs(item1[i] - item1[i - 1]));
+    }
+    return stoi(item2);
+}
+
