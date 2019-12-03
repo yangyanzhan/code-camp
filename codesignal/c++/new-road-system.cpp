efc7f717ba4f40de4addc54cd2215756887ec80a
yangyanzhan <yangyanzhan@gmail.com>
2019-12-03 14:33:03 +0800

    add Yanzhan's cpp solution for the new road system problem on codesignal

---
+++ codesignal/c++/new-road-system.cpp
@@ -0,0 +1,28 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/new-road-system.html .
+
+bool newRoadSystem(std::vector<std::vector<bool>> roadRegister) {
+    using namespace std;
+    int n = roadRegister.size();
+    for (int i = 0; i < n; i++) {
+        int outs = 0;
+        for (int j = 0; j < n; j++) {
+            if (roadRegister[i][j]) {
+                outs++;
+            }
+        }
+        int ins = 0;
+        for (int j = 0; j < n; j++) {
+            if (roadRegister[j][i]) {
+                ins++;
+            }
+        }
+        if (ins != outs) {
+            return false;
+        }
+    }
+    return true;
+}
+
