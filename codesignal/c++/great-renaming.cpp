a6f2165ed0d1b2ea9e867beff6042cd2800c6338
yangyanzhan <yangyanzhan@gmail.com>
2019-12-03 15:19:11 +0800

    add Yanzhan's cpp solution for the great renaming problem on codesignal

---
+++ codesignal/c++/great-renaming.cpp
@@ -0,0 +1,21 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/great-renaming.html .
+
+std::vector<std::vector<bool>> greatRenaming(std::vector<std::vector<bool>> roadRegister) {
+    using namespace std;
+    int n = roadRegister.size();
+    vector<vector<bool>> res;
+    for (int i = 0; i < n; i++) {
+        vector<bool> row;
+        for (int j = 0; j < n; j++) {
+            int i1 = (i - 1 + n) % n;
+            int j1 = (j - 1 + n) % n;
+            row.push_back(roadRegister[i1][j1]);
+        }
+        res.push_back(row);
+    }
+    return res;
+}
+
