d671c747ff67bfd1f3e7e7dde1a252b83f90cb1b
yangyanzhan <yangyanzhan@gmail.com>
2019-12-03 22:39:19 +0800

    add Yanzhan's cpp solution for the dot product problem on codesignal

---
+++ codesignal/c++/dot-product.cpp
@@ -0,0 +1,14 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/dot-product.html .
+
+int dotProduct(std::vector<int> v1, std::vector<int> v2) {
+    using namespace std;
+    int res = 0;
+    for (int i = 0; i < v1.size(); i++) {
+        res += v1[i] * v2[i];
+    }
+    return res;
+}
+
