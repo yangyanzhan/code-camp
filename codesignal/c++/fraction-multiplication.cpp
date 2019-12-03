1324d6ae080107a60ad7c007e8546feccaa9797c
yangyanzhan <yangyanzhan@gmail.com>
2019-12-03 11:28:06 +0800

    add Yanzhan's cpp solution for the fraction multiplication problem on codesignal

---
+++ codesignal/c++/fraction-multiplication.cpp
@@ -0,0 +1,19 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/fraction-multiplication.html .
+
+int gcd(int a, int b) {
+    if (a % b == 0) {
+        return b;
+    }
+    return gcd(b, a % b);
+}
+
+std::vector<int> fractionMultiplication(std::vector<int> a, std::vector<int> b) {
+    using namespace std;
+    int n1 = a[0] * b[0], n2 = a[1] * b[1];
+    int g = gcd(max(n1, n2), min(n1, n2));
+    return {n1 / g, n2 / g};
+}
+
