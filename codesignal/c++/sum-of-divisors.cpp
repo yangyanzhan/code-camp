78c245852493398d8718b9a660e241ea3f3ec8af
yangyanzhan <yangyanzhan@gmail.com>
2019-12-04 20:03:56 +0800

    add Yanzhan's cpp solution for the sum of divisors problem on codesignal

---
+++ codesignal/c++/sum-of-divisors.cpp
@@ -0,0 +1,19 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/sum-of-divisors.html .
+
+int sumOfDivisors(int n) {
+    int res = 0, half = sqrt(n);
+    for (int i = 1; i <= half; i++) {
+        if (n % i == 0) {
+            res += i;
+            res += n / i;
+        }
+    }
+    if (half * half == n) {
+        res -= half;
+    }
+    return res;
+}
+
