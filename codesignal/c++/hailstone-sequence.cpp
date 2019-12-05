fff67214df46e020585da20b8ce75ed26707a5de
yangyanzhan <yangyanzhan@gmail.com>
2019-12-05 21:49:16 +0800

    add Yanzhan's cpp solution for the hailstone sequence problem on codesignal

---
+++ codesignal/c++/hailstone-sequence.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/hailstone-sequence.html .
+
+int hailstoneSequence(int n) {
+    int step = 0;
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        step++;
+    }
+    return step;
+}
+
