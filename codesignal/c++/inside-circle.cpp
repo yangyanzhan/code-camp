a1619ce965c5cd4ddc197fb0a5d697a139c95529
yangyanzhan <yangyanzhan@gmail.com>
2019-12-05 11:34:18 +0800

    add Yanzhan's cpp solution for the inside circle problem on codesignal

---
+++ codesignal/c++/inside-circle.cpp
@@ -0,0 +1,11 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/inside-circle.html .
+
+bool insideCircle(std::vector<int> point, std::vector<int> center, int radius) {
+    using namespace std;
+    double a = point[0], b = point[1], c = center[0], d = center[1], dx = a - c, dy = b - d, r = radius;
+    return dx * dx + dy * dy <= r * r;
+}
+
