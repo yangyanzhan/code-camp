e23e6b29fedc22d439d566037a6683edf410a328
yangyanzhan <yangyanzhan@gmail.com>
2019-12-05 13:11:46 +0800

    add Yanzhan's cpp solution for the proper or improper problem on codesignal

---
+++ codesignal/c++/proper-or-improper.cpp
@@ -0,0 +1,10 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/proper-or-improper.html .
+
+std::string properOrImproper(std::vector<int> a) {
+    using namespace std;
+    return (abs(a[0]) < abs(a[1])) ? "Proper" : "Improper";
+}
+
