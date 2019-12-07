d59aa9a00a874fca78c07151b5ea62658bc3206b
yangyanzhan <yangyanzhan@gmail.com>
2019-12-07 15:20:08 +0800

    add Yanzhan's cpp solution for the unique digit products problem on codesignal

---
+++ codesignal/c++/unique-digit-products.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yangyanzhan.github.io .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yangyanzhan.github.io/codesignal/unique-digit-products.html .
+
+int uniqueDigitProducts(std::vector<int> a) {
+    using namespace std;
+    set<int> cache;
+    for (auto num: a) {
+        int product = 1;
+        while (num >= 10) {
+            product *= num % 10;
+            num /= 10;
+        }
+        product *= num;
+        cache.insert(product);
+    }
+    return cache.size();
+}
+
