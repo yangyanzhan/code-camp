4a9c5ae7053ea27977ae7dfcc19f14063e7637b8
yangyanzhan <yangyanzhan@gmail.com>
2019-12-07 23:22:33 +0800

    add Yanzhan's cpp solution for the count line colorings problem on codesignal

---
+++ codesignal/c++/count-line-colorings.cpp
@@ -0,0 +1,13 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/count-line-colorings.html .
+
+int countLineColorings(int points, int colors) {
+    int res = colors;
+    for (int i = 1; i < points; i++) {
+        res *= colors - 1;
+    }
+    return res;
+}
+
