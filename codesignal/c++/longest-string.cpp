c4eccea1b67c170b402ea200a38b3ce699e84888
yangyanzhan <yangyanzhan@gmail.com>
2019-12-07 16:54:31 +0800

    add Yanzhan's cpp solution for the longest string problem on codesignal

---
+++ codesignal/c++/longest-string.cpp
@@ -0,0 +1,16 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/longest-string.html .
+
+std::string longestString(std::vector<std::string> inputArray) {
+    using namespace std;
+    string best = "";
+    for (auto item: inputArray) {
+        if (item.size() > best.size()) {
+            best = item;
+        }
+    }
+    return best;
+}
+
