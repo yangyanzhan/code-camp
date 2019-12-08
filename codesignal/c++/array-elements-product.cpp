1f115401102af7354ad107fa81b3683282e8e69f
yangyanzhan <yangyanzhan@gmail.com>
2019-12-08 23:37:32 +0800

    add Yanzhan's cpp solution for the array elements product problem on codesignal

---
+++ codesignal/c++/array-elements-product.cpp
@@ -0,0 +1,13 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/array-elements-product.html .
+
+int arrayElementsProduct(std::vector<int> inputArray) {
+    int res = 1;
+    for (auto num: inputArray) {
+        res *= num;
+    }
+    return res;
+}
+
