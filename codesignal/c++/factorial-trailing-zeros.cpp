ba32220b4221ffe55805ca1e76d49b00a6322550
yangyanzhan <yangyanzhan@gmail.com>
2019-12-08 23:27:40 +0800

    add Yanzhan's cpp solution for the factorial trailing zeros problem on codesignal

---
+++ codesignal/c++/factorial-trailing-zeros.cpp
@@ -0,0 +1,14 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/factorial-trailing-zeros.html .
+
+int factorialTrailingZeros(int n) {
+    int res = 0;
+    while (n >= 5) {
+        n /= 5;
+        res += n;
+    }
+    return res;
+}
+
