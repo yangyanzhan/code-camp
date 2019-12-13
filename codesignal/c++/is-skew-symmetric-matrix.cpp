10c87253dad9422b4be8c2e5cbc11bd20f24ebf0
yangyanzhan <yangyanzhan@gmail.com>
2019-12-13 15:17:46 +0800

    add Yanzhan's cpp solution for the is skew symmetric matrix problem on codesignal

---
+++ codesignal/c++/is-skew-symmetric-matrix.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/is-skew-symmetric-matrix.html .
+
+bool isSkewSymmetricMatrix(std::vector<std::vector<int>> matrix) {
+    using namespace std;
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            if (matrix[i][j] != -matrix[j][i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
