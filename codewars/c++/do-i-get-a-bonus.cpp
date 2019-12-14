f455fbf20586374a1e0d20da7fd17dd3c5d3dd60
yangyanzhan <yangyanzhan@gmail.com>
2019-12-15 00:45:15 +0800

    add Yanzhan's cpp solution for the do i get a bonus problem on codewars

---
+++ codewars/c++/do-i-get-a-bonus.cpp
@@ -0,0 +1,13 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/do-i-get-a-bonus.html .
+
+#include<string>
+using namespace std;
+
+string bonus_time(int salary, bool bonus) {
+    string res = "$";
+    res += to_string(bonus ? (10 * salary) : salary);
+    return res;
+}
