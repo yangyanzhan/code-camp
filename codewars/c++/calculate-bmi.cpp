b8dba31abd89a24879e2b5812e2cb0986570659f
yangyanzhan <yangyanzhan@gmail.com>
2019-12-15 01:18:09 +0800

    add Yanzhan's cpp solution for the calculate bmi problem on codewars

---
+++ codewars/c++/calculate-bmi.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/calculate-bmi.html .
+
+std::string bmi(double w, double h) {
+    double bmi = (w * 100) / (h * 10 * h * 10);
+    if (bmi <= 18.5) {
+        return "Underweight";
+    } else if (bmi <= 25.0) {
+        return "Normal";
+    } else if (bmi <= 30.0) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+}
+
