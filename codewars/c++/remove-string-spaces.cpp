d880b1fd539aabe555a2ac463765e5be8e15a42e
yangyanzhan <yangyanzhan@gmail.com>
2019-12-14 23:36:37 +0800

    add Yanzhan's cpp solution for the remove string spaces problem on codewars

---
+++ codewars/c++/remove-string-spaces.cpp
@@ -0,0 +1,14 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/remove-string-spaces.html .
+
+#include <regex>
+
+std::string no_space(std::string x) {
+    using namespace std;
+    while (regex_search(x, regex(" "))) {
+        x = regex_replace(x, regex(" "), "");
+    }
+    return x;
+}
