fd65b0b56ea835c82a9335a1b05687ff2fb56109
yangyanzhan <yangyanzhan@gmail.com>
2019-12-16 14:15:19 +0800

    add Yanzhan's cpp solution for the square n sum problem on codewars

---
+++ codewars/c++/square-n-sum.cpp
@@ -0,0 +1,15 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/square-n-sum.html .
+
+#include <vector>
+
+int square_sum(const std::vector<int> &numbers) {
+    using namespace std;
+    int res = 0;
+    for (auto num: numbers) {
+        res += num * num;
+    }
+    return res;
+}
