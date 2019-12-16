cd39a8bed44a23801904afe420ef3939701c6455
yangyanzhan <yangyanzhan@gmail.com>
2019-12-16 11:18:07 +0800

    add Yanzhan's cpp solution for the trailing zeros problem on lintcode

---
+++ lintcode/c++/trailing-zeros.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/lintcode/trailing-zeros.html .
+
+class Solution {
+public:
+    /*
+     * @param n: A long integer
+     * @return: An integer, denote the number of trailing zeros in n!
+     */
+    long long trailingZeros(long long n) {
+        long long num5 = 0;
+        while (n > 0) {
+            n /= 5;
+            num5 += n;
+        }
+        return num5;
+    }
+};
