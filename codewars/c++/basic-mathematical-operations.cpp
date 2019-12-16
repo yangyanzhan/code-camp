d89df328f91a73e345882d3b72007e1885c14d59
yangyanzhan <yangyanzhan@gmail.com>
2019-12-16 15:39:07 +0800

    add Yanzhan's cpp solution for the basic mathematical operations problem on codewars

---
+++ codewars/c++/basic-mathematical-operations.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/basic-mathematical-operations.html .
+
+int basicOp(char op, int val1, int val2) {
+    switch (op) {
+        case '+':
+            return val1 + val2;
+        case '-':
+            return val1 - val2;
+        case '*':
+            return val1 * val2;
+        case '/':
+            return val1 / val2;
+    }
+    return -1;
+}
