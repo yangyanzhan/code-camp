e04525a4e2c70c60eceac9a8ba607e641229ac8f
yangyanzhan <yangyanzhan@gmail.com>
2019-12-16 15:26:22 +0800

    add Yanzhan's cpp solution for the convert boolean values to strings yes or no problem on codewars

---
+++ codewars/c++/convert-boolean-values-to-strings-yes-or-no.cpp
@@ -0,0 +1,10 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/convert-boolean-values-to-strings-yes-or-no.html .
+
+using namespace std;
+
+string bool_to_word(bool value) {
+    return value ? "Yes" : "No";
+}
