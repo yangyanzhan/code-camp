7ae9bf1e933ea0d74ba3367cf2d511cb5711b8f4
yangyanzhan <yangyanzhan@gmail.com>
2019-12-17 15:45:55 +0800

    add Yanzhan's cpp solution for the collatz conjecture problem on codewars

---
+++ codewars/c++/collatz-conjecture.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/collatz-conjecture.html .
+
+unsigned int hotpo(unsigned int n) {
+    if (n <= 1) {
+        return 0;
+    }
+    int res = 0;
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        res++;
+    }
+    return res;
+}
