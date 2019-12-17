ded85ab426f276f1c3533bad80b5c08ea3bc074b
yangyanzhan <yangyanzhan@gmail.com>
2019-12-17 14:01:56 +0800

    add Yanzhan's cpp solution for the playing with cubes 1 problem on codewars

---
+++ codewars/c++/playing-with-cubes-1.cpp
@@ -0,0 +1,17 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/playing-with-cubes-1.html .
+
+class Cube {
+    public:
+        int GetSide() {
+            return side;
+        }
+        void SetSide(int num) {
+            side = num;
+        }
+    private:
+        int side = 0;
+};
+
