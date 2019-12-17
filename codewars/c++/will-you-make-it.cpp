111debc153322d3251a992ae9b02af94e9955bf5
yangyanzhan <yangyanzhan@gmail.com>
2019-12-17 14:54:18 +0800

    add Yanzhan's cpp solution for the will you make it problem on codewars

---
+++ codewars/c++/will-you-make-it.cpp
@@ -0,0 +1,9 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/will-you-make-it.html .
+
+bool zero_fuel(uint32_t distance_to_pump, uint32_t mpg, uint32_t fuel_left) {
+    return mpg * fuel_left >= distance_to_pump;
+}
+
