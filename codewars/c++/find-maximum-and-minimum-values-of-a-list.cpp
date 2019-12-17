eda019dc0332921fb7b14b18143c843e540b8fde
yangyanzhan <yangyanzhan@gmail.com>
2019-12-17 18:09:56 +0800

    add Yanzhan's cpp solution for the find maximum and minimum values of a list problem on codewars

---
+++ codewars/c++/find-maximum-and-minimum-values-of-a-list.cpp
@@ -0,0 +1,17 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/find-maximum-and-minimum-values-of-a-list.html .
+
+#include <vector>
+
+using namespace std;
+
+int min(vector<int> list) {
+    return *min_element(list.begin(), list.end());
+}
+
+int max(vector<int> list) {
+    return *max_element(list.begin(), list.end());
+}
+
