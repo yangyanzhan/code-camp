543a6b304856a7bcc22a86d802a4bb18f3d5f948
yangyanzhan <yangyanzhan@gmail.com>
2019-12-17 12:49:58 +0800

    add Yanzhan's cpp solution for the fake binary problem on codewars

---
+++ codewars/c++/fake-binary.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/fake-binary.html .
+
+#include <string>
+
+std::string fakeBin(std::string str) {
+    using namespace std;
+    string res = str;
+    for (auto &ch: res) {
+        if (ch < '5') {
+            ch = '0';
+        } else {
+            ch = '1';
+        }
+    }
+    return res;
+}
+
