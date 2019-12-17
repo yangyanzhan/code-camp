3b13139a54b6f861b30e076e894c673299fc9cdd
yangyanzhan <yangyanzhan@gmail.com>
2019-12-17 12:54:03 +0800

    add Yanzhan's cpp solution for the is the string uppercase problem on codewars

---
+++ codewars/c++/is-the-string-uppercase.cpp
@@ -0,0 +1,15 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/is-the-string-uppercase.html .
+
+#include <string>
+
+bool is_uppercase(const std::string &s) {
+    for (auto ch: s) {
+        if (toupper(ch) != ch) {
+            return false;
+        }
+    }
+    return true;
+}
