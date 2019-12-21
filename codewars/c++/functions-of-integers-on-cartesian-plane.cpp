a9bf9a5d223bc77b3fb27ceb36dc58a585644007
yangyanzhan <yangyanzhan@gmail.com>
2019-12-21 22:48:21 +0800

    add Yanzhan's cpp solution for the functions of integers on cartesian plane problem on codewars

---
+++ codewars/c++/functions-of-integers-on-cartesian-plane.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/functions-of-integers-on-cartesian-plane.html .
+
+using namespace std;
+
+class Funij {
+  public:
+  static unsigned long long sumin(unsigned long long n) {
+      return n * (n + 1) * (2 * n + 1) / 6;
+  }
+  static unsigned long long sumax(unsigned long long n) {
+      return n * n * (n + 1) - n * (n + 1) * (2 * n + 1) / 6;
+  }
+  static unsigned long long sumsum(unsigned long long n) {
+      auto m = n * (n + 1) / 2;
+      return n * m + n * m;
+  }
+};
