9e23266f58dcf3db685afb3acc85109f0132e247
yangyanzhan <yangyanzhan@gmail.com>
2019-12-22 00:28:57 +0800

    add Yanzhan's cpp solution for the deodorant evaporator problem on codewars

---
+++ codewars/c++/deodorant-evaporator.cpp
@@ -0,0 +1,16 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/deodorant-evaporator.html .
+
+#include <cmath>
+
+using namespace std;
+
+class Evaporator {
+  public:
+    static int evaporator(double content, double evap_per_day, double threshold) {
+        return ceil(log(threshold / 100.0) / log(1 - evap_per_day / 100));
+    }
+};
+
