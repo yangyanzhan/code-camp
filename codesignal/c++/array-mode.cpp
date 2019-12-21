328606325ea2e55869266d5e060c0b70bd528e0c
yangyanzhan <yangyanzhan@gmail.com>
2019-12-21 15:06:33 +0800

    add Yanzhan's cpp solution for the array mode problem on codesignal

---
+++ codesignal/c++/array-mode.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/array-mode.html .
+
+int arrayMode(std::vector<int> sequence) {
+    using namespace std;
+    map<int, int> cache;
+    for (auto num: sequence) {
+        if (cache.find(num) == cache.end()) {
+            cache[num] = 0;
+        }
+        cache[num]++;
+    }
+    int res = 0, count = 0;
+    for (auto pair: cache) {
+        if (pair.second > count) {
+            count = pair.second;
+            res = pair.first;
+        }
+    }
+    return res;
+}
+
