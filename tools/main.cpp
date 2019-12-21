26326cc7cac7096f6756b3806ca76a63c975b5e6
yangyanzhan <yangyanzhan@gmail.com>
2019-12-21 13:20:11 +0800

    refine toolchain

---
+++ tools/main.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <set>
+#include <map>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <chrono>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <limits>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
