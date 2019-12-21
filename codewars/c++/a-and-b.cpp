6c06ee294838555c7f476e2369aa642b4e983860
yangyanzhan <yangyanzhan@gmail.com>
2019-12-21 22:25:38 +0800

    add Yanzhan's cpp solution for the a and b problem on codewars

---
+++ codewars/c++/a-and-b.cpp
@@ -0,0 +1,9 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/a-and-b.html .
+
+int testit(int a, int b) {
+    return a | b;
+}
+
