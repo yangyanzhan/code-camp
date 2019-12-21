1c127d90030e9737621d381035200be8c33c2541
yangyanzhan <yangyanzhan@gmail.com>
2019-12-21 12:56:22 +0800

    add Yanzhan's cpp solution for the is perfect score problem on codesignal

---
+++ codesignal/c++/is-perfect-score.cpp
@@ -0,0 +1,9 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/is-perfect-score.html .
+
+bool isPerfectScore(bool ans1, bool ans2, bool ans3) {
+    return ans1 && ans2 && ans3;
+}
+
