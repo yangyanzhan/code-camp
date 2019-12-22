a6fbe85589606b5d35763b9f1bc72b31b6321e9d
yangyanzhan <yangyanzhan@gmail.com>
2019-12-23 00:24:22 +0800

    add Yanzhan's cpp solution for the complementary dna problem on codewars

---
+++ codewars/c++/complementary-dna.cpp
@@ -0,0 +1,19 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/complementary-dna.html .
+
+#include <vector>
+#include <string>
+
+std::string DNAStrand(const std::string& dna) {
+    using namespace std;
+    string res = dna;
+    vector<string> cache{"ATGC", "TACG"};
+    for (auto &ch: res) {
+        int idx = cache[0].find(ch);
+        ch = cache[1][idx];
+    }
+    return res;
+}
+
