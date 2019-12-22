ff2bc3c43f7962428a7dd166f074ab8d7c6d421c
yangyanzhan <yangyanzhan@gmail.com>
2019-12-23 00:07:53 +0800

    add Yanzhan's cpp solution for the is identity matrix problem on codesignal

---
+++ codesignal/c++/is-identity-matrix.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/is-identity-matrix.html .
+
+bool isIdentityMatrix(std::vector<std::vector<int>> matrix) {
+    using namespace std;
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                if (matrix[i][j] != 0) {
+                    return false;
+                }
+            } else {
+                if (matrix[i][j] != 1) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
