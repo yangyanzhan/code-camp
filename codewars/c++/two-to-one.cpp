ac0ddae47888b0565a487e55b5e094913ca8dc2c
yangyanzhan <yangyanzhan@gmail.com>
2019-12-22 23:31:03 +0800

    add Yanzhan's cpp solution for the two to one problem on codewars

---
+++ codewars/c++/two-to-one.cpp
@@ -0,0 +1,23 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/two-to-one.html .
+
+#include <set>
+#include <algorithm>
+
+class TwoToOne {
+public:
+    static std::string longest(const std::string &s1, const std::string &s2) {
+        using namespace std;
+        set<char> cache;
+        for (auto ch: s1) {
+            cache.insert(ch);
+        }
+        for (auto ch: s2) {
+            cache.insert(ch);
+        }
+        return string(cache.begin(), cache.end());
+    }
+};
+
