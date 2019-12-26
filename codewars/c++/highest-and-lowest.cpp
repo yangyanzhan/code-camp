a7f3e99a7effe496990b204565750bd73bfc30be
yangyanzhan <yangyanzhan@gmail.com>
2019-12-26 21:58:05 +0800

    add Yanzhan's cpp solution for the highest and lowest problem on codewars

---
+++ codewars/c++/highest-and-lowest.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/highest-and-lowest.html .
+
+#include <regex>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+std::string highAndLow(const std::string& numbers){
+    using namespace std;
+    regex reg("-?\\d+");
+    vector<int> nums;
+    for (auto it = sregex_iterator(numbers.begin(), numbers.end(), reg); it != sregex_iterator(); it++) {
+        nums.push_back(stoi(it->str()));
+    }
+    sort(nums.begin(), nums.end());
+    stringstream ss;
+    ss << nums.back() << " " << nums.front();
+    return ss.str();
+}
+
