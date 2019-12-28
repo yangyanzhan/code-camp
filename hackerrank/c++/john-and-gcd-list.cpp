5fb502c0de50ffd8ce34da5b18ceea1fd4247b90
yangyanzhan <yangyanzhan@gmail.com>
2019-12-29 01:03:06 +0800

    add Yanzhan's cpp solution for the john and gcd list problem on hackerrank

---
+++ hackerrank/c++/john-and-gcd-list.cpp
@@ -0,0 +1,30 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/hackerrank/john-and-gcd-list.html .
+
+int gcd(int a, int b) {
+    if (a < b) {
+        swap(a, b);
+    }
+    if (a % b == 0) {
+        return b;
+    }
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+vector<int> solve(vector<int> nums) {
+    vector<int> res{nums[0]};
+    for (int i = 0; i < nums.size() - 1; i++) {
+        int a = res[i], b = nums[i + 1], t = nums[i];
+        int c = lcm(b, t);
+        res.push_back(c);
+    }
+    res.push_back(nums.back());
+    return res;
+}
+
