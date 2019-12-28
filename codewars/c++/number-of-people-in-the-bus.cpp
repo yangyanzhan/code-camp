65f2b45016312cb59d4e7ba9d22100019d9685cd
yangyanzhan <yangyanzhan@gmail.com>
2019-12-28 22:00:24 +0800

    add Yanzhan's cpp solution for the number of people in the bus problem on codewars

---
+++ codewars/c++/number-of-people-in-the-bus.cpp
@@ -0,0 +1,17 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/number-of-people-in-the-bus.html .
+
+#include <utility>
+#include <vector>
+
+unsigned int number(const std::vector<std::pair<int, int>> &busStops) {
+    using namespace std;
+    int res = 0;
+    for (auto stop: busStops) {
+        res += get<0>(stop) - get<1>(stop);
+    }
+    return res;
+}
+
