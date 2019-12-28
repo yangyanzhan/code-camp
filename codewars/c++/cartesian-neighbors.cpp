be5469cfa72bec6eb9d7c82fd73529e3aac698c6
yangyanzhan <yangyanzhan@gmail.com>
2019-12-28 22:58:14 +0800

    add Yanzhan's cpp solution for the cartesian neighbors problem on codewars

---
+++ codewars/c++/cartesian-neighbors.cpp
@@ -0,0 +1,21 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/cartesian-neighbors.html .
+
+#include <vector>
+using std::vector;
+
+vector<vector<int>> cartesianNeighbor(int x, int y) {
+    vector<vector<int>> res;
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            if (i == 0 && j == 0) {
+                continue;
+            }
+            res.push_back({x + i, y + j});
+        }
+    }
+    return res;
+}
+
