f77debbca29b1ff845d44354a9981af5318cc44c
yangyanzhan <yangyanzhan@gmail.com>
2019-12-29 22:49:38 +0800

    add Yanzhan's cpp solution for the ugly number 2 problem on lintcode

---
+++ lintcode/c++/ugly-number-2.cpp
@@ -0,0 +1,26 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/lintcode/ugly-number-2.html .
+
+class Solution {
+public:
+    /**
+     * @param n: An integer
+     * @return: return a  integer as description.
+     */
+    int nthUglyNumber(int n) {
+        vector<int> res(1, 1);
+        int i2 = 0, i3 = 0, i5 = 0;
+        while (res.size() < n) {
+            int m2 = res[i2] * 2, m3 = res[i3] * 3, m5 = res[i5] * 5;
+            int mn = min(m2, min(m3, m5));
+            if (mn == m2) ++i2;
+            if (mn == m3) ++i3;
+            if (mn == m5) ++i5;
+            res.push_back(mn);
+        }
+        return res.back();
+    }
+};
+
