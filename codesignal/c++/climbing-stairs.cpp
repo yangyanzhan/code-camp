04a7a7f9f26d84458e9f065622ca806008773639
yangyanzhan <yangyanzhan@gmail.com>
2020-01-01 18:14:15 +0800

    add Yanzhan's cpp solution for the climbing stairs problem on codesignal

---
+++ codesignal/c++/climbing-stairs.cpp
@@ -0,0 +1,13 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/climbing-stairs.html .
+
+int climbingStairs(int n) {
+    int cache[n + 1] = {1, 1};
+    for (int i = 2; i <= n; i++) {
+        cache[i] = cache[i - 1] + cache[i - 2];
+    }
+    return cache[n];
+}
+
