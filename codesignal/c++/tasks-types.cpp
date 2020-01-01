515a5da2926e95b1499dc7a463127e96d1477687
yangyanzhan <yangyanzhan@gmail.com>
2020-01-01 18:08:22 +0800

    add Yanzhan's cpp solution for the tasks types problem on codesignal

---
+++ codesignal/c++/tasks-types.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/tasks-types.html .
+
+std::vector<int> tasksTypes(std::vector<int> deadlines, int day) {
+    using namespace std;
+    int today = 0, upcoming = 0, later = 0;
+    for (auto num: deadlines) {
+        if (num <= day) {
+            today++;
+        } else if (num <= day + 7) {
+            upcoming++;
+        } else {
+            later++;
+        }
+    }
+    return {today, upcoming, later};
+}
+
