e8d3c6082a6f63c3348177c952821f51be89960b
yangyanzhan <yangyanzhan@gmail.com>
2020-01-08 23:40:20 +0800

    add Yanzhan's cpp solution for the triangle existence problem on codesignal

---
+++ codesignal/c++/triangle-existence.cpp
@@ -0,0 +1,10 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/triangle-existence.html .
+
+bool triangleExistence(std::vector<int> sides) {
+    int a = sides[0], b = sides[1], c = sides[2];
+    return ((a + b) > c) && ((a + c) > b) && ((b + c) > a);
+}
+
