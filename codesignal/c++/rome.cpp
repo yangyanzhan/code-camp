f0d34cdeb72072f6c2a7b5df63855ef011b8a571
yangyanzhan <yangyanzhan@gmail.com>
2020-01-11 23:25:05 +0800

    add Yanzhan's cpp solution for the rome problem on codesignal

---
+++ codesignal/c++/rome.cpp
@@ -0,0 +1,23 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/rome.html .
+
+std::string rome(int n) {
+    using namespace std;
+    vector<int> bases = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    vector<string> names = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    string res;
+    int i = 0;
+    while (n) {
+        int base = bases[i];
+        string name = names[i];
+        while (n >= base) {
+            n -= base;
+            res += name;
+        }
+        i++;
+    }
+    return res;
+}
+
