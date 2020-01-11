ebc14b71eec209d60e36bae035b13f46b7175be4
yangyanzhan <yangyanzhan@gmail.com>
2020-01-11 16:12:05 +0800

    add Yanzhan's cpp solution for the mean scores problem on codesignal

---
+++ codesignal/c++/mean-scores.cpp
@@ -0,0 +1,16 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/mean-scores.html .
+
+std::vector<int> meanScores(std::vector<int> scores) {
+    using namespace std;
+    vector<int> res;
+    int sum = 0;
+    for (int i = 0; i < scores.size(); i++) {
+        sum += scores[i];
+        res.push_back(sum / (i + 1));
+    }
+    return res;
+}
+
