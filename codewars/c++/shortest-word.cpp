b0ffa7dfe36695755fab51624e2cce9a0384f8f6
yangyanzhan <yangyanzhan@gmail.com>
2020-01-18 16:25:23 +0800

    add Yanzhan's cpp solution for the shortest word problem on codewars

---
+++ codewars/c++/shortest-word.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/shortest-word.html .
+
+#include <regex>
+
+int find_short(std::string str) {
+    using namespace std;
+    regex reg("[a-zA-Z]+");
+    int min_len = numeric_limits<int>::max();
+    for (auto it = sregex_iterator(str.begin(), str.end(), reg); it != sregex_iterator(); it++) {
+        string item = it->str();
+        int len = item.size();
+        min_len = min(min_len, len);
+    }
+    return min_len;
+}
