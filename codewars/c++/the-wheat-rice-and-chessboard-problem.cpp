508c544dba99b79f1213cd05730797a51974cea1
yangyanzhan <yangyanzhan@gmail.com>
2020-01-19 23:29:59 +0800

    add Yanzhan's cpp solution for the the wheat rice and chessboard problem problem on codewars

---
+++ codewars/c++/the-wheat-rice-and-chessboard-problem.cpp
@@ -0,0 +1,12 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/the-wheat-rice-and-chessboard-problem.html .
+
+#include <cmath>
+
+unsigned int squaresNeeded(long long n) {
+    using namespace std;
+    return ceil(log2(n + 1));
+}
+
