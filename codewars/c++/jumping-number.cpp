7650c87ef7ba5646920b6fb35908385fddcc898f
yangyanzhan <yangyanzhan@gmail.com>
2020-01-24 11:51:02 +0800

    add Yanzhan's cpp solution for the jumping number problem on codewars

---
+++ codewars/c++/jumping-number.cpp
@@ -0,0 +1,19 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/jumping-number.html .
+
+#include <string>
+
+using namespace std;
+
+string jumpingNumber(int number) {
+    auto item = to_string(number);
+    for (int i = 1; i < item.size(); i++) {
+        if (abs(item[i] - item[i - 1]) != 1) {
+            return "Not!!";
+        }
+    }
+    return "Jumping!!";
+}
+
