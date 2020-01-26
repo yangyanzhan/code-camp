9f12689d48f07733add958f66a82ea3e72b087e1
yangyanzhan <yangyanzhan@gmail.com>
2020-01-26 10:35:10 +0800

    add Yanzhan's cpp solution for the special number problem on codewars

---
+++ codewars/c++/special-number.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/special-number.html .
+
+#include <string>
+
+using namespace std;
+
+string specialNumber(int number) {
+    string t = "012345";
+    string item = to_string(number);
+    for (auto ch: item) {
+        if (t.find(ch) == string::npos) {
+            return "NOT!!";
+        }
+    }
+    return "Special!!";
+}
+
