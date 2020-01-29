c485ea9e7088cc17feaddb28574b1e6b4f7a816d
yangyanzhan <yangyanzhan@gmail.com>
2020-01-29 08:44:58 +0800

    add Yanzhan's cpp solution for the form the minimum problem on codewars

---
+++ codewars/c++/form-the-minimum.cpp
@@ -0,0 +1,19 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/form-the-minimum.html .
+
+#include <vector>
+
+using namespace std;
+
+unsigned long long minValue(vector<int> values) {
+    sort(values.begin(), values.end());
+    values.resize(distance(values.begin(), unique(values.begin(), values.end())));
+    string res = "";
+    for (auto v: values) {
+        res += to_string(v);
+    }
+    return stoull(res);
+}
+
