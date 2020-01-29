4c2db7fd0622585fbbfe554f367c13404201ae48
yangyanzhan <yangyanzhan@gmail.com>
2020-01-29 09:54:53 +0800

    add Yanzhan's cpp solution for the add two numbers problem on leetcode

---
+++ leetcode/c++/add-two-numbers.cpp
@@ -0,0 +1,44 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/add-two-numbers.html .
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        auto head = new ListNode(0), curr = head;
+        while (l1 && l2) {
+            int sum = l1->val + l2->val + carry;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        auto l = l1 ? l1 : l2;
+        while (l) {
+            int sum = l->val + carry;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+            l = l->next;
+        }
+        if (carry) {
+            curr->next = new ListNode(carry);
+        }
+        auto res = head->next;
+        delete head;
+        return res;
+    }
+};
+
