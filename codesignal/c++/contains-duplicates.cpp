5fc1122d8ea0d1ba30dcd7d7a3dd76733d45f1e8
yangyanzhan <yangyanzhan@gmail.com>
2020-01-29 09:05:41 +0800

    add Yanzhan's cpp solution for the contains duplicates problem on codesignal

---
+++ codesignal/c++/contains-duplicates.cpp
@@ -0,0 +1,17 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codesignal/contains-duplicates.html .
+
+bool containsDuplicates(std::vector<int> a) {
+    using namespace std;
+    set<int> cache;
+    for (auto num: a) {
+        if (cache.find(num) != cache.end()) {
+            return true;
+        }
+        cache.insert(num);
+    }
+    return false;
+}
+
