cb8040ed3558da2392ee95908d797ff87b1be4d8
yangyanzhan <yangyanzhan@gmail.com>
2020-01-29 08:36:12 +0800

    add Yanzhan's cpp solution for the maximum triplet sum problem on codewars

---
+++ codewars/c++/maximum-triplet-sum.cpp
@@ -0,0 +1,16 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/maximum-triplet-sum.html .
+
+#include <vector>
+
+using namespace std;
+
+int maxTriSum(vector<int> numbers) {
+    sort(numbers.begin(), numbers.end());
+    numbers.resize(distance(numbers.begin(), unique(numbers.begin(), numbers.end())));
+    int n = numbers.size();
+    return numbers[n - 3] + numbers[n - 2] + numbers[n - 1];
+}
+
