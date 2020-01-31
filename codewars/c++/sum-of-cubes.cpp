35a420d278b70156eca3acea2c8688bed32ee2d1
yangyanzhan <yangyanzhan@gmail.com>
2020-01-31 17:49:06 +0800

    add Yanzhan's cpp solution for the sum of cubes problem on codewars

---
+++ codewars/c++/sum-of-cubes.cpp
@@ -0,0 +1,13 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/sum-of-cubes.html .
+
+unsigned int sum_cubes(unsigned int n) {
+    long long res = 0;
+    for (long long i = 1; i <= n; i++) {
+        res += i * i * i;
+    }
+    return res;
+}
+
