f13ea4aa38d72b1e87fa27e291b2e73b78b73549
yangyanzhan <yangyanzhan@gmail.com>
2020-02-11 00:23:34 +0800

    add Yanzhan's cpp solution for the tower of hanoi problem on codewars

---
+++ codewars/c++/tower-of-hanoi.cpp
@@ -0,0 +1,13 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/tower-of-hanoi.html .
+
+long long towerOfHanoi(int rings) {
+    long long res = 0;
+    while (rings-- > 0) {
+        res = 2 * res + 1;
+    }
+    return res;
+}
+
