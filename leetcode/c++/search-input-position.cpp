53530ced878f071d59c1c309696848c135ef11f5
yangyanzhan <yangyanzhan@gmail.com>
2020-02-19 00:20:17 +0800

    add Yanzhan's cpp solution for the search input position problem on leetcode

---
+++ leetcode/c++/search-input-position.cpp
@@ -0,0 +1,13 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/search-input-position.html .
+
+class Solution {
+public:
+    int searchInsert(vector<int> &nums, int target) {
+        auto it = lower_bound(nums.begin(), nums.end(), target);
+        return distance(nums.begin(), it);
+    }
+};
+
