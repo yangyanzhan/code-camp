847d9c9db5136b1e424ec1584f084b29bdcaf40a
yangyanzhan <yangyanzhan@gmail.com>
2020-02-23 13:12:52 +0800

    add Yanzhan's cpp solution for the climbing stairs problem on leetcode

---
+++ leetcode/c++/climbing-stairs.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/climbing-stairs.html .
+
+class Solution {
+    map<int, int> cache;
+public:
+    int climbStairs(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n <= 1) {
+            return 1;
+        }
+        if (cache.find(n) != cache.end()) {
+            return cache[n];
+        }
+        int res = climbStairs(n - 1) + climbStairs(n - 2);
+        cache[n] = res;
+        return res;
+    }
+};
+
