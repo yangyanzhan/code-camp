90f38d64b99b0d9443a1e07d990da57cc8c1ae26
yangyanzhan <yangyanzhan@gmail.com>
2020-03-01 01:35:15 +0800

    add Yanzhan's cpp solution for the find numbers with even number of digits problem on leetcode

---
+++ leetcode/c++/find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/find-numbers-with-even-number-of-digits.html .
+
+class Solution {
+public:
+    int findNumbers(vector<int> &nums) {
+        int res = 0;
+        for (auto &num: nums) {
+            if (to_string(num).size() % 2 == 0) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
+
