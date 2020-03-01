324d7369427381e06809a78b22287e10259d8f9b
yangyanzhan <yangyanzhan@gmail.com>
2020-03-02 01:36:38 +0800

    add Yanzhan's cpp solution for the convert binary number in a linked list to integer problem on leetcode

---
+++ leetcode/c++/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -0,0 +1,26 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/convert-binary-number-in-a-linked-list-to-integer.html .
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    int getDecimalValue(ListNode *head) {
+        int res = 0;
+        while (head) {
+            res = 2 * res + head->val;
+            head = head->next;
+        }
+        return res;
+    }
+};
+
