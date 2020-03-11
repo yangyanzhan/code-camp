92e55b1c508df32123f26cca60283510b2e56e2e
yangyanzhan <yangyanzhan@gmail.com>
2020-03-11 12:36:38 +0800

    add Yanzhan's cpp solution for the to lower case problem on leetcode

---
+++ leetcode/c++/to-lower-case.cpp
@@ -0,0 +1,15 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/to-lower-case.html .
+
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for (auto &ch : str) {
+            ch = tolower(ch);
+        }
+        return str;
+    }
+};
+
