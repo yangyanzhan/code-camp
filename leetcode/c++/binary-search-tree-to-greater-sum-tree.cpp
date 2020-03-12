c258c2ea96cbae8cd8e277477610f5a1c16e9fe7
yangyanzhan <yangyanzhan@gmail.com>
2020-03-12 17:57:34 +0800

    add Yanzhan's cpp solution for the binary search tree to greater sum tree problem on leetcode

---
+++ leetcode/c++/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,30 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/binary-search-tree-to-greater-sum-tree.html .
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+    int sum = 0;
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        if (!root) {
+            return nullptr;
+        }
+        bstToGst(root->right);
+        sum += root->val;
+        root->val = sum;
+        bstToGst(root->left);
+        return root;
+    }
+};
+
