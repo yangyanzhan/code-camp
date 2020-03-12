a46fbbee3561d8f09136e60251ba246ba7b6610c
yangyanzhan <yangyanzhan@gmail.com>
2020-03-12 12:47:49 +0800

    add Yanzhan's cpp solution for the maximum 69 number problem on leetcode

---
+++ leetcode/c++/maximum-69-number.cpp
@@ -0,0 +1,19 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/maximum-69-number.html .
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        auto item = to_string(num);
+        for (auto &ch: item) {
+            if (ch == '6') {
+                ch = '9';
+                break;
+            }
+        }
+        return stoi(item);
+    }
+};
+
