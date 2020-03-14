3c91b08747efedd24e01a7b37668a855b1ded815
yangyanzhan <yangyanzhan@gmail.com>
2020-03-14 12:25:37 +0800

    add Yanzhan's cpp solution for the remove outermost parentheses problem on leetcode

---
+++ leetcode/c++/remove-outermost-parentheses.cpp
@@ -0,0 +1,27 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/remove-outermost-parentheses.html .
+
+class Solution {
+public:
+    string removeOuterParentheses(string S) {
+        int count = 0;
+        string res;
+        for (auto ch: S) {
+            if (ch == '(') {
+                if (count > 0) {
+                    res += ch;
+                }
+                count++;
+            } else {
+                if (count > 1) {
+                    res += ch;
+                }
+                count--;
+            }
+        }
+        return res;
+    }
+};
+
