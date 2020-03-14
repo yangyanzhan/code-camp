520da2292a2046121e6dd71f7ad254b32228b884
yangyanzhan <yangyanzhan@gmail.com>
2020-03-14 12:34:56 +0800

    add Yanzhan's cpp solution for the decrypt string from alphabet to integer mapping problem on leetcode

---
+++ leetcode/c++/decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/decrypt-string-from-alphabet-to-integer-mapping.html .
+
+class Solution {
+public:
+    string freqAlphabets(string s) {
+        int idx = string::npos;
+        string res;
+        while ((idx = s.find("#")) != string::npos) {
+            for (int i = 0; i < idx - 2; i++) {
+                res += 'a' + s[i] - '1';
+            }
+            res += 'a' + stoi(s.substr(idx - 2, 2)) - 1;
+            s = s.substr(idx + 1);
+        }
+        for (int i = 0; i < s.size(); i++) {
+            res += 'a' + s[i] - '1';
+        }
+        return res;
+    }
+};
+
