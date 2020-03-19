77f00a9a18d03552256a0f3a7024e2c82aafa467
yangyanzhan <yangyanzhan@gmail.com>
2020-03-19 19:40:54 +0800

    add Yanzhan's cpp solution for the find n unique integers sum up to zero problem on leetcode

---
+++ leetcode/c++/find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/find-n-unique-integers-sum-up-to-zero.html .
+
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> res;
+        for (int i = 1; i <= n / 2; i++) {
+            res.push_back(-i);
+            res.push_back(i);
+        }
+        if (n % 2 != 0) {
+            res.push_back(0);
+        }
+        return res;
+    }
+};
+
