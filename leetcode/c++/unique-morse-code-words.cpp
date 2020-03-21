2c53e60a907bee1529fc2c93f4b34679ea24db5a
yangyanzhan <yangyanzhan@gmail.com>
2020-03-21 12:10:53 +0800

    add Yanzhan's cpp solution for the unique morse code words problem on leetcode

---
+++ leetcode/c++/unique-morse-code-words.cpp
@@ -0,0 +1,21 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/unique-morse-code-words.html .
+
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string> symbols = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        set<string> cache;
+        for (auto &word: words) {
+            string item;
+            for (auto &ch: word) {
+                item += symbols[ch - 'a'];
+            }
+            cache.insert(item);
+        }
+        return cache.size();
+    }
+};
+
