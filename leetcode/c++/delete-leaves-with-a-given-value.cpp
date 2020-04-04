edc38906f528e36d39aec02cf40f944a9f7fa9ea
yangyanzhan <yangyanzhan@gmail.com>
2020-04-04 22:19:55 +0800

    add Yanzhan's cpp solution for the delete leaves with a given value problem on leetcode

---
+++ leetcode/c++/delete-leaves-with-a-given-value.cpp
@@ -0,0 +1,32 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/delete-leaves-with-a-given-value.html .
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+        if (root->left == nullptr && root->right == nullptr) {
+            if (root->val == target) {
+                return nullptr;
+            }
+        }
+        return root;
+    }
+};
+
