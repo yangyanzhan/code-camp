52ffcc68f156e92adf2100585191c4de073ecc0c
yangyanzhan <yangyanzhan@gmail.com>
2020-04-05 11:04:36 +0800

    add Yanzhan's cpp solution for the n ary tree preorder traversal problem on leetcode

---
+++ leetcode/c++/n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,41 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/n-ary-tree-preorder-traversal.html .
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> res;
+        if (root == nullptr) {
+            return res;
+        }
+        res.push_back(root->val);
+        for (auto child: root->children) {
+            auto sub = preorder(child);
+            res.insert(res.end(), sub.begin(), sub.end());
+        }
+        return res;
+    }
+};
+
