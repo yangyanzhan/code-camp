094abf77b58ea2dff84b6877a2fa7f8127421a23
yangyanzhan <yangyanzhan@gmail.com>
2020-04-07 12:34:17 +0800

    add Yanzhan's cpp solution for the find positive integer solution for a given equation problem on leetcode

---
+++ leetcode/c++/find-positive-integer-solution-for-a-given-equation.cpp
@@ -0,0 +1,35 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/find-positive-integer-solution-for-a-given-equation.html .
+
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ * public:
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     int f(int x, int y);
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+        vector<vector<int>> res;
+        for (int i = 1; i <= 1000; i++) {
+            for (int j = 1; j <= 1000; j++) {
+                int v = customfunction.f(i, j);
+                if (v == z) {
+                    res.push_back({i, j});
+                } else if (v > z) {
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
+
