07d763bc65f2a69f35a46ad2a4e649089696599a
yangyanzhan <yangyanzhan@gmail.com>
2020-04-07 15:32:57 +0800

    add Yanzhan's cpp solution for the maximum depth of n ary tree problem on leetcode

---
+++ leetcode/c++/maximum-depth-of-n-ary-tree.cpp
@@ -0,0 +1,40 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/maximum-depth-of-n-ary-tree.html .
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        int depth = 0;
+        if (root == nullptr) {
+            return depth;
+        }
+        for (auto child: root->children) {
+            depth = max(depth, maxDepth(child));
+        }
+        depth++;
+        return depth;
+    }
+};
+
