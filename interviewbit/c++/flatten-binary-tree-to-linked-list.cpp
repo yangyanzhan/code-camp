c68678f37a511395c7202a0cf2ae3bfbb175eba8
yangyanzhan <yangyanzhan@gmail.com>
2020-07-10 18:22:18 +0800

    add Yanzhan's solution for the flatten binary tree to linked list problem on interviewbit

---
+++ interviewbit/c++/flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/interviewbit/flatten-binary-tree-to-linked-list.html .
+
+TreeNode* Solution::flatten(TreeNode* A) {
+    stack<TreeNode *> nodes;
+    TreeNode *curr = A;
+    while (curr) {
+        if (curr->right) {
+            nodes.push(curr->right);
+        }
+        if (curr->left) {
+            curr->right = curr->left;
+            curr->left = NULL;
+        } else if (!nodes.empty()) {
+            TreeNode *right = nodes.top();
+            nodes.pop();
+            curr->right = right;
+        }
+        curr = curr->right;
+    }
+    return A;
+}
