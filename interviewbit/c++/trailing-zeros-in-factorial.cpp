737de5f3fe25fbdf62f37a2125a84c0b69c7625f
yangyanzhan <yangyanzhan@gmail.com>
2020-07-10 15:05:18 +0800

    add Yanzhan's solution for the trailing zeros in factorial problem on interviewbit

---
+++ interviewbit/c++/trailing-zeros-in-factorial.cpp
@@ -0,0 +1,14 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/interviewbit/trailing-zeros-in-factorial.html .
+
+int Solution::trailingZeroes(int A) {
+    int res = 0;
+    while (A > 0) {
+        A /= 5;
+        res += A;
+    }
+    return res;
+}
+
