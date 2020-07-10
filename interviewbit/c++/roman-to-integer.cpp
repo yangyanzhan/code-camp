f6cff33d56131164111f17c4c11ec9105a435f9e
yangyanzhan <yangyanzhan@gmail.com>
2020-07-10 15:36:15 +0800

    add Yanzhan's solution for the roman to integer problem on interviewbit

---
+++ interviewbit/c++/roman-to-integer.cpp
@@ -0,0 +1,22 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/interviewbit/roman-to-integer.html .
+
+int Solution::romanToInt(string A) {
+    vector<string> words = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    vector<int> nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    int idx = 0, res = 0;
+    while (A.length() > 0) {
+        string key = words[idx];
+        int num = nums[idx];
+        if (A.find(key) != 0) {
+            idx++;
+        } else {
+            res += num;
+            A = A.substr(key.length());
+        }
+    }
+    return res;
+}
+
