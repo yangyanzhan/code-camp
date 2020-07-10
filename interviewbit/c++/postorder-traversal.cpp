bc45a42f21872de7c5d31bbce2b44998eb50cf2d
yangyanzhan <yangyanzhan@gmail.com>
2020-07-10 18:42:52 +0800

    add Yanzhan's solution for the postorder traversal problem on interviewbit

---
+++ interviewbit/c++/postorder-traversal.cpp
@@ -0,0 +1,25 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/interviewbit/postorder-traversal.html .
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<int> Solution::postorderTraversal(TreeNode* A) {
+    if (!A) {
+        return vector<int>();
+    }
+    vector<int> first = postorderTraversal(A->left);
+    vector<int> second = postorderTraversal(A->right);
+    first.insert(first.end(), second.begin(), second.end());
+    first.push_back(A->val);
+    return first;
+}
+
