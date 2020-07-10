557fc8958feaef1e31428f9236092e4a6f335a28
yangyanzhan <yangyanzhan@gmail.com>
2020-07-10 17:52:46 +0800

    add Yanzhan's solution for the ways to decode problem on interviewbit

--- interviewbit/c++/ways-to-decode.cpp
+++ interviewbit/c++/ways-to-decode.cpp
@@ -3,26 +3,22 @@
 // It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
 // Blog URL for this problem: https://yanzhan.site/interviewbit/ways-to-decode.html .
 
-int Solution::numDecodings(string s) {
-    int n = s.length(), ppre = 1, pre = 1;
-    if (n <= 0) {
+int ways(const string& A, int s) {
+    if(s == A.size())
+        return 1;
+    else if(A[s] == '0')
         return 0;
-    }
-    for (int i = 0; i < n; i++) {
-        int now = 0;
-        char ch = s[i];
-        if ('1' <= ch && ch <= '9') {
-            now += pre;
-        }
-        if (i >= 1) {
-            int num = stoi(s.substr(i - 1, 2));
-            if (10 <= num && num <= 26) {
-                now += ppre;
-            }
-        }
-        ppre = pre;
-        pre = now;
-    }
-    return pre;
+    else if(A[s] > '2')
+        return ways(A, s + 1);
+    else if(s + 1 >= A.size())
+        return ways(A, s + 1);
+    else if(A[s] == '1' || (A[s] == '2' && A[s + 1] < '7'))
+        return ways(A, s + 1) + ways(A, s + 2);
+    else
+        return ways(A, s + 1);
 }
 
+
+int Solution::numDecodings(string A) {
+    return ways(A, 0);
+}
