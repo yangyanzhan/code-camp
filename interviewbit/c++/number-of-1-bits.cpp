4d76cf835695bece95d91417a67799b457b4bab2
yangyanzhan <yangyanzhan@gmail.com>
2020-07-10 17:14:53 +0800

    add Yanzhan's solution for the number of 1 bits problem on interviewbit

---
+++ interviewbit/c++/number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/interviewbit/number-of-1-bits.html .
+
+int Solution::numSetBits(unsigned int A) {
+    int res = 0;
+    while (A > 0) {
+        res += (A % 2);
+        A /= 2;
+    }
+    return res;
+}
+
