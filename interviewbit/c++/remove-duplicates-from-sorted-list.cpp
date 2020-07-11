d14719b13e143896709b9af72f5750b2b1fec731
yangyanzhan <yangyanzhan@gmail.com>
2020-07-11 17:18:29 +0800

    add Yanzhan's solution for the remove duplicates from sorted list problem on interviewbit

---
+++ interviewbit/c++/remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,27 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/interviewbit/remove-duplicates-from-sorted-list.html .
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode *Solution::deleteDuplicates(ListNode *A) {
+    if (A == NULL)
+        return NULL;
+    ListNode *head = A, *curr = head, *ptr = A->next;
+    while (ptr) {
+        if (ptr->val != curr->val) {
+            curr->next = ptr;
+            curr = curr->next;
+        }
+        ptr = ptr->next;
+    }
+    curr->next = NULL;
+    return head;
+}
