e4c5a96577e97fa305f4d9d9078838e8ba629cf5
yangyanzhan <yangyanzhan@gmail.com>
2020-07-17 12:34:07 +0800

    add Yanzhan's solution for the sum of angles problem on codewars

---
+++ codewars/c++/sum-of-angles.cpp
@@ -0,0 +1,6 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/sum-of-angles.html .
+
+int angle(int n) { return (n - 2) * 180; }
