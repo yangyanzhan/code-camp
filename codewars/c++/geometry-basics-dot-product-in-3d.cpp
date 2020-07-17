3f75b7e3926973046035678d446cc94dd51cbf66
yangyanzhan <yangyanzhan@gmail.com>
2020-07-17 12:07:18 +0800

    add Yanzhan's solution for the geometry basics dot product in 3d problem on codewars

---
+++ codewars/c++/geometry-basics-dot-product-in-3d.cpp
@@ -0,0 +1,9 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/geometry-basics-dot-product-in-3d.html .
+
+double dot_product(const Vector &a, const Vector &b) {
+    double res = a.x * b.x + a.y * b.y + a.z * b.z;
+    return res;
+}
