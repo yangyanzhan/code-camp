0d9ad71c7a86a26da1706beef9c410cace0ee5d7
yangyanzhan <yangyanzhan@gmail.com>
2020-07-17 12:21:52 +0800

    add Yanzhan's solution for the simple fun 3 late ride problem on codewars

---
+++ codewars/c++/simple-fun-3-late-ride.cpp
@@ -0,0 +1,10 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/simple-fun-3-late-ride.html .
+
+int lateRide(int n) {
+    int h = n / 60;
+    int m = n % 60;
+    return h / 10 + h % 10 + m / 10 + m % 10;
+}
