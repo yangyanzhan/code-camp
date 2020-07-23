43fecdd5960999088851c561c26a66f5d81bf7c5
yangyanzhan <yangyanzhan@gmail.com>
2020-07-24 00:25:22 +0800

    add Yanzhan's solution for the which section did you scroll to problem on codewars

---
+++ codewars/c++/which-section-did-you-scroll-to.cpp
@@ -0,0 +1,18 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/which-section-did-you-scroll-to.html .
+
+#include <vector>
+
+using namespace std;
+
+int get_section_id(int n, std::vector<int> a) {
+    for (int i = 0; i < a.size(); i++) {
+        if (n < a[i]) {
+            return i;
+        }
+        n -= a[i];
+    }
+    return -1;
+}
