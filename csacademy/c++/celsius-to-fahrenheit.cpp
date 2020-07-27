30f808d64a83cc303d1476cb677e736ebdec0a66
yangyanzhan <yangyanzhan@gmail.com>
2020-07-27 12:46:12 +0800

    add Yanzhan's solution for the celsius to fahrenheit problem on csacademy

---
+++ csacademy/c++/celsius-to-fahrenheit.cpp
@@ -0,0 +1,16 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/celsius-to-fahrenheit.html .
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int c;
+    cin >> c;
+    int f = (c * 9.0) / 5 + 32;
+    cout << f;
+    return 0;
+}
