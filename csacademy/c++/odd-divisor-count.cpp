05bdcb20cbfbf9b35b8315472ed0fb59409506c9
yangyanzhan <yangyanzhan@gmail.com>
2020-07-27 11:44:53 +0800

    add Yanzhan's solution for the odd divisor count problem on csacademy

---
+++ csacademy/c++/odd-divisor-count.cpp
@@ -0,0 +1,23 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/odd-divisor-count.html .
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int res = 0;
+    for (int n = a; n <= b; n++) {
+        int m = sqrt(n);
+        if (m * m == n) {
+            res++;
+        }
+    }
+    cout << res;
+    return 0;
+}
