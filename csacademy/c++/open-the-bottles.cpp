9143b85f149c09f02ef2be107a89c7a9b8089097
yangyanzhan <yangyanzhan@gmail.com>
2020-07-29 11:50:13 +0800

    add Yanzhan's solution for the open the bottles problem on csacademy

---
+++ csacademy/c++/open-the-bottles.cpp
@@ -0,0 +1,37 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/open-the-bottles.html .
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int res = 0;
+    for (int i = 0; i < 3; i++) {
+        vector<int> nums(3, 0);
+        for (int j = 0; j < 3; j++) {
+            cin >> nums[j];
+        }
+        res += *min_element(nums.begin(), nums.end());
+    }
+    cout << res;
+    return 0;
+}
