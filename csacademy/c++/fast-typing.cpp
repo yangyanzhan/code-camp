9251bb62275ca482db7d8fbc8d57dfe42b4b2463
yangyanzhan <yangyanzhan@gmail.com>
2020-07-29 12:28:09 +0800

    add Yanzhan's solution for the fast typing problem on csacademy

---
+++ csacademy/c++/fast-typing.cpp
@@ -0,0 +1,39 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/fast-typing.html .
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll res = 0;
+    string item;
+    cin >> item;
+    vector<ll> nums(26, 0);
+    for (int i = 0; i < 26; i++) {
+        cin >> nums[i];
+    }
+    for (auto ch : item) {
+        res += nums[ch - 'a'];
+    }
+    cout << res;
+    return 0;
+}
