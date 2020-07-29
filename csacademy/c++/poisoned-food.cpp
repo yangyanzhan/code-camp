e0cc9d2470168911d350eeef98309b04b1a5f6d7
yangyanzhan <yangyanzhan@gmail.com>
2020-07-29 23:48:49 +0800

    add Yanzhan's solution for the poisoned food problem on csacademy

---
+++ csacademy/c++/poisoned-food.cpp
@@ -0,0 +1,43 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/poisoned-food.html .
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n;
+    cin >> n;
+    int res = 0;
+    double maxi = numeric_limits<int>::min();
+    for (int i = 0; i < n; i++) {
+        double t, p;
+        cin >> t >> p;
+        double ratio = (t - p) / t;
+        if (ratio > maxi) {
+            res = i + 1;
+            maxi = ratio;
+        }
+    }
+    cout << res;
+    return 0;
+}
