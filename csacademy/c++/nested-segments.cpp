5e1991d149d834f2939e1f24644caf1a6439391f
yangyanzhan <yangyanzhan@gmail.com>
2020-07-29 23:52:33 +0800

    add Yanzhan's solution for the nested segments problem on csacademy

---
+++ csacademy/c++/nested-segments.cpp
@@ -0,0 +1,56 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/nested-segments.html .
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> segs(n);
+    for (int i = 0; i < n; i++) {
+        int l, r;
+        cin >> l >> r;
+        segs[i] = vector<int>{l, r};
+    }
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        bool found = false;
+        int l = segs[i][0];
+        int r = segs[i][1];
+        for (int j = 0; j < n; j++) {
+            if (j == i) {
+                continue;
+            }
+            if (segs[j][0] < l && r < segs[j][1]) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            res++;
+        }
+    }
+    cout << res;
+    return 0;
+}
