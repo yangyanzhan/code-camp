f7debf80e5112ea037c295f5cb61933d4c9d35d4
yangyanzhan <yangyanzhan@gmail.com>
2020-07-29 10:40:31 +0800

    add Yanzhan's solution for the one letter problem on csacademy

---
+++ csacademy/c++/one-letter.cpp
@@ -0,0 +1,40 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/one-letter.html .
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n;
+    cin >> n;
+    stringstream ss;
+    for (int i = 0; i < n; i++) {
+        string word;
+        cin >> word;
+        auto ch = *min_element(word.begin(), word.end());
+        ss << (char)ch;
+    }
+    auto item = ss.str();
+    sort(item.begin(), item.end());
+    cout << item;
+    return 0;
+}
