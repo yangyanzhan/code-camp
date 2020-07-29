614ed99f827c1cfd5c11648e85c2852425102edf
yangyanzhan <yangyanzhan@gmail.com>
2020-07-29 23:45:33 +0800

    add Yanzhan's solution for the reversed number problem on csacademy

---
+++ csacademy/c++/reversed-number.cpp
@@ -0,0 +1,35 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/reversed-number.html .
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll x;
+    cin >> x;
+    auto item = to_string(x);
+    reverse(item.begin(), item.end());
+    ll y = stoll(item);
+    cout << (x < y ? 1 : 0);
+    return 0;
+}
