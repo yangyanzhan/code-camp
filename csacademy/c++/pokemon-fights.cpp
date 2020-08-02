001027f27267e9cebefa22022e0b8c4f64e168b6
yangyanzhan <yangyanzhan@gmail.com>
2020-08-02 19:48:50 +0800

    add Yanzhan's solution for the pokemon fights problem on csacademy

---
+++ csacademy/c++/pokemon-fights.cpp
@@ -0,0 +1,59 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/pokemon-fights.html .
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// #include <execution>
+#include <exception>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    vector<int> res(n, 0);
+    int curr = 0;
+    for (int i = 1; i < n; i++) {
+        if (nums[i] > nums[i - 1]) {
+            res[i]++;
+            curr = i;
+        } else {
+            res[curr]++;
+            swap(nums[i], nums[i - 1]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << res[i] << " ";
+    }
+    return 0;
+}
