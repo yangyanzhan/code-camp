6136eb5954b45d0b5ad40ace9d4019cc393a7afc
yangyanzhan <yangyanzhan@gmail.com>
2020-08-02 13:56:43 +0800

    add Yanzhan's solution for the bicycle rental problem on csacademy

---
+++ csacademy/c++/bicycle-rental.cpp
@@ -0,0 +1,48 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/bicycle-rental.html .
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <deque>
+#include <exception>
+// #include <execution>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n;
+    cin >> n;
+    int res = numeric_limits<int>::max();
+    for (int i = 0; i < n; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        res = min(res, max(a, b) + c);
+    }
+    cout << res << endl;
+    return 0;
+}
