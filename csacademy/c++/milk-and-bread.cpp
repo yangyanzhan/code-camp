1b896833e3e3f5f0bd3047ba2b6e68cb63d2e2fa
yangyanzhan <yangyanzhan@gmail.com>
2020-08-02 11:40:34 +0800

    add Yanzhan's solution for the milk and bread problem on csacademy

---
+++ csacademy/c++/milk-and-bread.cpp
@@ -0,0 +1,32 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/milk-and-bread.html .
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int x1, x2, x3;
+    cin >> x1 >> x2 >> x3;
+    cout << abs(x1 - x2) + min(abs(x3 - x1), abs(x3 - x2));
+    return 0;
+}
