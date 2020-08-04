677af66e85a168db7d4392427dde18621f0e43bd
yangyanzhan <yangyanzhan@gmail.com>
2020-08-04 11:33:51 +0800

    add Yanzhan's solution for the money machine problem on csacademy

---
+++ csacademy/c++/money-machine.cpp
@@ -0,0 +1,57 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/csacademy/money-machine.html .
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// #include <execution>
+#include <exception>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    ll n, c, x1, u, x2;
+    cin >> n >> c >> x1 >> u >> x2;
+    ll res = c;
+    ll inc = x1;
+    for (int i = 1; i <= n; i++) {
+        if (res >= u) {
+            if ((n - i + 1) * x2 > u) {
+                res -= u;
+                inc += x2;
+            } else {
+                res += inc * (n - i + 1);
+                break;
+            }
+        }
+        res += inc;
+    }
+    cout << res << endl;
+    return 0;
+}
