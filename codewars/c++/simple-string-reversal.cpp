30a93a0f6408fba292965770fb1d3c867f93249d
yangyanzhan <yangyanzhan@gmail.com>
2020-08-09 20:46:21 +0800

    add Yanzhan's solution for the simple string reversal problem on codewars

---
+++ codewars/c++/simple-string-reversal.cpp
@@ -0,0 +1,24 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/simple-string-reversal.html .
+
+using namespace std;
+
+std::string solve(string s) {
+    stringstream ss;
+    int n = s.size();
+    int idx = n - 1;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == ' ') {
+            ss << " ";
+        } else {
+            while (s[idx] == ' ') {
+                idx--;
+            }
+            ss << (char)s[idx];
+            idx--;
+        }
+    }
+    return ss.str();
+}
