b8aeeeef58f7510594e677de0c948a3bfb2336ae
yangyanzhan <yangyanzhan@gmail.com>
2020-08-16 13:28:12 +0800

    add Yanzhan's solution for the oh come on hello world once again problem on codewars

---
+++ codewars/c++/oh-come-on-hello-world-once-again.cpp
@@ -0,0 +1,10 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/oh-come-on-hello-world-once-again.html .
+
+std::string f() {
+    std::string s = {0110, 0145, 0154, 0154, 0157, 054, 040,
+                     0167, 0157, 0162, 0154, 0144, 041};
+    return s;
+}
