150ccd839dbc45baa9f347a1ff9b76614c655e65
yangyanzhan <yangyanzhan@gmail.com>
2020-08-16 15:50:12 +0800

    add Yanzhan's solution for the frompascaltocpp problem on codewars

---
+++ codewars/c++/frompascaltocpp.cpp
@@ -0,0 +1,21 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/frompascaltocpp.html .
+
+using namespace std;
+
+#define begin {
+#define repeat do {
+#define until(COND)                                                            \
+    }                                                                          \
+    while (!(COND))
+#define end }
+
+int fromPascalToCpp(int Sum, int finish, int start) {
+    begin repeat Sum = Sum + start;
+    start = start + 1;
+    until(start >= finish);
+    end;
+    return Sum;
+}
