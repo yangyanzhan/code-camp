db43b177e1e1049ff0f1d40af949ba075e5cc16c
yangyanzhan <yangyanzhan@gmail.com>
2020-08-16 15:16:50 +0800

    add Yanzhan's solution for the bus mastering who is the most prioritary problem on codewars

---
+++ codewars/c++/bus-mastering-who-is-the-most-prioritary.cpp
@@ -0,0 +1,20 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/bus-mastering-who-is-the-most-prioritary.html .
+
+using namespace std;
+
+string arbitrate(const string &input, int n) {
+    bool found = false;
+    string output = input;
+    for (int i = 0; i < n; i++) {
+        if (found) {
+            output[i] = '0';
+        }
+        if (output[i] == '1') {
+            found = true;
+        }
+    }
+    return output;
+}
