e40db3065f1fbd292d2b24749263c480d6d52926
yangyanzhan <yangyanzhan@gmail.com>
2020-08-16 15:19:05 +0800

    add Yanzhan's solution for the matrix creation problem on codewars

---
+++ codewars/c++/matrix-creation.cpp
@@ -0,0 +1,16 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/codewars/matrix-creation.html .
+
+using namespace std;
+
+std::vector<std::vector<int>> getMatrix(const int n) {
+    vector<vector<int>> res;
+    for (int i = 0; i < n; i++) {
+        vector<int> row(n, 0);
+        row[i] = 1;
+        res.push_back(row);
+    }
+    return res;
+}
