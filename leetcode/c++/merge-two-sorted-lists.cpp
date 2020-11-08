cfdbbe86e925873827af34dba4e508dc30860723
yangyanzhan <yangyanzhan@gmail.com>
2020-11-08 11:25:47 +0800

    add Yanzhan's solution for the merge two sorted lists problem on leetcode

---
+++ leetcode/c++/merge-two-sorted-lists.cpp
@@ -0,0 +1,41 @@
+// Hi, I'm Yanzhan. For more algothmic problems, visit my Youtube Channel (Yanzhan Yang's Youtube Channel) : https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber or my Twitter Account (Yanzhan Yang's Twitter) : https://twitter.com/YangYanzhan or my GitHub HomePage (Yanzhan Yang's GitHub HomePage) : https://yanzhan.site .
+// For this specific algothmic problem, visit my Youtube Video :  .
+// It's fascinating to solve algothmic problems, follow Yanzhan to learn more!
+// Blog URL for this problem: https://yanzhan.site/leetcode/merge-two-sorted-lists.html .
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode *head = new ListNode(), *curr = head;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                curr->next = l1;
+                curr = curr->next;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                curr = curr->next;
+                l2 = l2->next;
+            }
+        }
+        if (l1) {
+            curr->next = l1;
+        }
+        if (l2) {
+            curr->next = l2;
+        }
+        return head->next;
+    }
+};
